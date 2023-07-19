868d259a4cd1f8751736fbbcfb3e6438e21bdf6c
Krishna <krishna.gangimeni@improve10x.com>
2023-07-19 11:43:51 +0530

    Displayed Krishna In Demo Class

--- src/java/com/improve10x/tdd/Demo.java
+++ src/java/com/improve10x/tdd/Demo.java
@@ -4,5 +4,6 @@ public class Demo {
     public static void main(String[] args) {
         System.out.println("Team D");
         System.out.println("Supriya");
+        System.out.println("Krishna");
     }
 }
