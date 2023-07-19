3bc33b31236c0311730b1ee531f9b4f8ff81a111
Krishna <krishna.gangimeni@improve10x.com>
2023-07-19 15:37:06 +0530

    Added code for FarmProblem

---
+++ src/java/com/improve10x/tdd/krishna/farmproblem/FarmProblem.java
@@ -0,0 +1,9 @@
+package com.improve10x.tdd.krishna.farmproblem;
+
+public class FarmProblem {
+
+    public int findFarmProblem(int chicken, int cows, int pigs){
+
+        return (chicken*2 + cows*4 + pigs*4);
+    }
+}
