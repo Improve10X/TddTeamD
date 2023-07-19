af1610aa5f4538fa4acb30fb6e74d76f6ba9b3ca
Vani <vani.pushpala@improve10x.com>
2023-07-19 15:54:04 +0530

    Added code for Frames per second

---
+++ src/tests/com/improve10x/tdd/vani/framespersecond/FramesPerSecondTest.java
@@ -0,0 +1,38 @@
+package com.improve10x.tdd.vani.framespersecond;
+
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
+
+import static org.junit.jupiter.api.Assertions.assertEquals;
+
+public class FramesPerSecondTest {
+    FramesPerSecond framesPerSecond;
+
+    @BeforeEach
+    public void setUp() {
+        framesPerSecond = new FramesPerSecond();
+    }
+
+    @Test
+    public void nothing() {
+    }
+
+    @Test
+    public void givenOneOne_returnsSeconds() {
+        int seconds = framesPerSecond.frames(1, 1);
+        assertEquals(60, seconds);
+    }
+
+    @Test
+    public void givenTenOne_returnsSeconds() {
+        int seconds = framesPerSecond.frames(10, 1);
+        assertEquals(600, seconds);
+    }
+
+    @Test
+    public void givenTenTwentyFive_returnsSeconds() {
+        int seconds = framesPerSecond.frames(10, 25);
+        assertEquals(15000, seconds);
+    }
+
+}
