fd05ddd6d2b1057ac4d16f8f3896154f3eea3536
Supriya <supriya.singamsetty@improve10x.com>
2023-07-19 11:27:22 +0530

    Displayed Supriya In Demo Class

--- src/java/com/improve10x/tdd/Demo.java
+++ src/java/com/improve10x/tdd/Demo.java
@@ -3,5 +3,6 @@ package com.improve10x.tdd;
 public class Demo {
     public static void main(String[] args) {
         System.out.println("Team D");
+        System.out.println("Supriya");
     }
 }
