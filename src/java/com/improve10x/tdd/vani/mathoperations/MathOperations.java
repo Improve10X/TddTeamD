ecd72930a44e326c5ed11fc7884505ff861f8c73
Vani <vani.pushpala@improve10x.com>
2023-07-19 15:11:26 +0530

    Added code for math operations

---
+++ src/java/com/improve10x/tdd/vani/mathoperations/MathOperations.java
@@ -0,0 +1,51 @@
+package com.improve10x.tdd.vani.mathoperations;
+
+public class MathOperations {
+
+//    public int add(int a, int b){
+//        if (a > 0 && b > 0){
+//            return a + b;
+//        }
+//        if (a > 0){
+//            return a;
+//        }else if(b > 0) {
+//            return b;
+//        }
+//        return 0;
+
+    public int add(int a, int b){
+        return a + b;
+    }
+
+//    public int sub(int a, int b){
+//        if (a > 0 && b > 0){
+//            return a - b;
+//        }
+//        if (a > 0){
+//            return a;
+//        }else if(b > 0){
+//            return -b;
+//        }
+//        return 0;
+//    }
+
+    public int sub(int a, int b){
+        return a - b;
+    }
+
+//    public int multi(int a, int b){
+//        if (a == b){
+//            return a * b;
+//        }
+//        if (a > b){
+//            return a;
+//        } else if (b > a) {
+//            return b;
+//        }
+//        return 0;
+//    }
+
+    public int multi (int a, int b){
+        return a * b;
+    }
+}
