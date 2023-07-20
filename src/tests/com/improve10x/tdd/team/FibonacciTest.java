e3c3124c27244388223b5ee940e332e7df6247ba
Krishna <krishna.gangimeni@improve10x.com>
2023-07-20 11:38:46 +0530

    Added testcase and logic for input minus two

--- src/tests/com/improve10x/tdd/team/FibonacciTest.java
+++ src/tests/com/improve10x/tdd/team/FibonacciTest.java
@@ -29,6 +29,12 @@ public class FibonacciTest {
         Fibonacci fibonacci = new Fibonacci();
         int zero = fibonacci.find(1);
         assertEquals(0, zero);
+    }
 
+    @Test
+    public void givenMinusTwo_returnsMinusOne(){
+        Fibonacci fibonacci = new Fibonacci();
+        int minusOne = fibonacci.find(-2);
+        assertEquals(-1, minusOne);
     }
 }
