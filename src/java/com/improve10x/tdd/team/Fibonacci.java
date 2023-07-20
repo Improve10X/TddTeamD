19bbac04a879e95ba88948653d749be4d4d978cc
Viswanath Kumar Sandu <vissu.info@gmail.com>
2023-07-20 10:55:53 +0530

    Added givenMinus1_returnsMinusOne test case for Fibonacci

---
+++ src/java/com/improve10x/tdd/team/Fibonacci.java
@@ -0,0 +1,7 @@
+package com.improve10x.tdd.team;
+
+public class Fibonacci {
+    public int find(int n) {
+        return -1;
+    }
+}
--- src/tests/com/improve10x/tdd/team/FibonacciTest.java
+++ src/tests/com/improve10x/tdd/team/FibonacciTest.java
@@ -1,9 +1,19 @@
 package com.improve10x.tdd.team;
 
+import org.junit.jupiter.api.Assertions;
 import org.junit.jupiter.api.Test;
+
+import static org.junit.jupiter.api.Assertions.assertEquals;
 
 public class FibonacciTest {
 
     @Test
     public void nothing() {}
+
+    @Test
+    public void givenMinus1_returnsMinusOne() {
+        Fibonacci fibonacci = new Fibonacci();
+        int minusOne = fibonacci.find(-1);
+        assertEquals(-1, minusOne);
+    }
 }
