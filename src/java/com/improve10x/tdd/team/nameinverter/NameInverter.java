14806d94dd1deb41e749bf5a1be9a1ba47cf47a6
Vani <vani.pushpala@improve10x.com>
2023-07-21 16:47:08 +0530

    Added test case and logic for input honorifics

--- src/java/com/improve10x/tdd/team/nameinverter/NameInverter.java
+++ src/java/com/improve10x/tdd/team/nameinverter/NameInverter.java
@@ -1,5 +1,8 @@
 package com.improve10x.tdd.team.nameinverter;
 
+import java.util.ArrayList;
+import java.util.Arrays;
+
 public class NameInverter {
     public String invertName(String name) {
         if (name.equals("")) {
@@ -7,8 +10,13 @@ public class NameInverter {
         } else if (!name.trim().contains(" ")){
             return name.trim();
         }else {
-            String[] names = name.trim().split("\\s+");
-            return names[1] + ", " + names[0];
+            ArrayList<String> names =  new ArrayList<>(Arrays.asList(name.trim().split("\\s+")));
+            for (int i = 0; i < names.size(); i++){
+                if (names.get(0).matches("ms.|mrs.|miss.")){
+                    names.remove(0);
+                }
+            }
+            return names.get(1) + ", " + names.get(0);
         }
     }
 }
