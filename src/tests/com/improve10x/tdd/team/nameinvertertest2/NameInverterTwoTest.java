3f4a8c0c0a5ba0ffd1aeceb77e2298e480697911
Krishna <krishna.gangimeni@improve10x.com>
2023-07-21 16:37:04 +0530

    Added testcase and logic for input Empty

--- src/tests/com/improve10x/tdd/team/nameinvertertest2/NameInverterTwoTest.java
+++ src/tests/com/improve10x/tdd/team/nameinvertertest2/NameInverterTwoTest.java
@@ -1,7 +1,20 @@
 package com.improve10x.tdd.team.nameinvertertest2;
 
+import com.improve10x.tdd.team.nameinverter.NameInverter;
+import com.improve10x.tdd.team.nameinverter2.NameInverterTwo;
 import org.junit.jupiter.api.Test;
+
+import static org.junit.jupiter.api.Assertions.assertEquals;
 
 public class NameInverterTwoTest {
 
+    @Test
+    public void nothing(){
+    }
+    @Test
+    public void givenEmpty_returnsEmpty(){
+        NameInverterTwo nameInverterTwo = new NameInverterTwo();
+        String invertedName = nameInverterTwo.invertName("");
+        assertEquals("", invertedName);
+    }
 }
