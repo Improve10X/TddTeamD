83bc8e3e6c1e8d36a0ec40711f87c9b6f2e3510e
Vani <vani.pushpala@improve10x.com>
2023-07-21 15:41:18 +0530

    Added test case and logic for input single word

--- src/java/com/improve10x/tdd/team/nameinverter/NameInverter.java
+++ src/java/com/improve10x/tdd/team/nameinverter/NameInverter.java
@@ -2,6 +2,11 @@ package com.improve10x.tdd.team.nameinverter;
 
 public class NameInverter {
     public String invertName(String name) {
-        return "";
+        if (name.equals("")) {
+            return "";
+        } else if (!name.contains(" ")) {
+            return name;
+        }
+        return null;
     }
 }
