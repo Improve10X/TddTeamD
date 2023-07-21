cc2e12a724d98665821332ae3b4a03bf27f1a1b9
Supriya <supriya.singamsetty@improve10x.com>
2023-07-21 16:23:12 +0530

    Added TestCase And Logic For Input Empty

--- src/java/com/improve10x/tdd/team/nameinverterone/NameInverterOne.java
+++ src/java/com/improve10x/tdd/team/nameinverterone/NameInverterOne.java
@@ -1,4 +1,10 @@
 package com.improve10x.tdd.team.nameinverterone;
 
 public class NameInverterOne {
+    public String invertName(String word) {
+        if (word.equals("")){
+            return "";
+        }
+        return null;
+    }
 }
--- src/tests/com/improve10x/tdd/team/nameinverterone/NameInverterOneTest.java
+++ src/tests/com/improve10x/tdd/team/nameinverterone/NameInverterOneTest.java
@@ -1,4 +1,16 @@
 package com.improve10x.tdd.team.nameinverterone;
 
+import org.junit.jupiter.api.Assertions;
+import org.junit.jupiter.api.Test;
+
 public class NameInverterOneTest {
+    @Test
+    public void nothing(){}
+
+    @Test
+    public void givenEmpty_returnEmpty(){
+        NameInverterOne nameInverterOne = new NameInverterOne();
+        String invertedName = nameInverterOne.invertName("");
+        Assertions.assertEquals("", invertedName);
+    }
 }
