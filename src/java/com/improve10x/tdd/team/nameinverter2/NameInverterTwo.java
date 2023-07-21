b805d28264b1680d24bcd40c93eb797f0ec240fd
Krishna <krishna.gangimeni@improve10x.com>
2023-07-21 16:10:24 +0530

    Added testcase and logic for input three four one

---
+++ src/java/com/improve10x/tdd/team/nameinverter2/NameInverterTwo.java
@@ -0,0 +1,4 @@
+package com.improve10x.tdd.team.nameinverter2;
+
+public class NameInverterTwo {
+}
---
+++ src/tests/com/improve10x/tdd/team/nameinvertertest2/NameInverterTwoTest.java
@@ -0,0 +1,7 @@
+package com.improve10x.tdd.team.nameinvertertest2;
+
+import org.junit.jupiter.api.Test;
+
+public class NameInverterTwoTest {
+
+}
