c477203791e74c95ca65129e00a87b7e11c87b33
Supriya <supriya.singamsetty@improve10x.com>
2023-07-21 16:27:55 +0530

    Added TestCase And Logic For ScoringSystem

--- src/tests/com/improve10x/tdd/team/scoringone/ScoringSystemTest.java
+++ src/tests/com/improve10x/tdd/team/scoringone/ScoringSystemTest.java
@@ -13,27 +13,27 @@ import static org.junit.jupiter.api.Assertions.assertEquals;
 //ABBACCCCAC->
 public class ScoringSystemTest {
 
-//    @Test
-//    public void nothing(){
-//
-//    }
-//    @Test
-//    public void givensA_returnsOneZerZero(){
-//        ScoringSystem scoringSystem = new ScoringSystem();
-//        int[] onlyAScore = scoringSystem.calculates("A");
-//        assertArrayEquals(new int[]{1,0,0},onlyAScore );
-//    }
-//    @Test
-//    public void givensABC_returnsOneOneOne(){
-//        ScoringSystem scoringSystem = new ScoringSystem();
-//        int[] Score = scoringSystem.calculates("ABC");
-//        assertArrayEquals(new int[]{1,1,1},Score);
-//    }
-//    @Test
-//    public void givensABBACCCCAC_returnsOneOneOne(){
-//        ScoringSystem scoringSystem = new ScoringSystem();
-//        int[] Score = scoringSystem.calculates("ABBCCCCAC");
-//        assertArrayEquals(new int[]{2,2,5},Score);
-//    }
+    @Test
+    public void nothing(){
+
+    }
+    @Test
+    public void givensA_returnsOneZerZero(){
+        ScoringSystem scoringSystem = new ScoringSystem();
+        int[] onlyAScore = scoringSystem.calculates("A");
+        assertArrayEquals(new int[]{1,0,0},onlyAScore );
+    }
+    @Test
+    public void givensABC_returnsOneOneOne(){
+        ScoringSystem scoringSystem = new ScoringSystem();
+        int[] Score = scoringSystem.calculates("ABC");
+        assertArrayEquals(new int[]{1,1,1},Score);
+    }
+    @Test
+    public void givensABBACCCCAC_returnsOneOneOne(){
+        ScoringSystem scoringSystem = new ScoringSystem();
+        int[] Score = scoringSystem.calculates("ABBCCCCAC");
+        assertArrayEquals(new int[]{2,2,5},Score);
+    }
 
 }
