8dbe4adc138f2d9840cd71070df5244cf519341b
Krishna <krishna.gangimeni@improve10x.com>
2023-07-21 11:50:25 +0530

    Added testcase and logic for input null

---
+++ src/java/com/improve10x/tdd/team/scoring/Scoring.java
@@ -0,0 +1,7 @@
+package com.improve10x.tdd.team.scoring;
+
+public class Scoring {
+    public int[] calculateScores(String score) {
+        return new int[3];
+    }
+}
---
+++ src/tests/com/improve10x/tdd/team/scoringtest/ScoringTest.java
@@ -0,0 +1,22 @@
+package com.improve10x.tdd.team.scoringtest;
+
+import com.improve10x.tdd.team.scoring.Scoring;
+import org.junit.jupiter.api.Test;
+
+import java.util.Arrays;
+
+import static org.junit.jupiter.api.Assertions.assertArrayEquals;
+
+public class ScoringTest {
+
+    @Test
+    public void nothing(){
+
+    }
+    @Test
+    public void givenNull_returnsAllZeros(){
+        Scoring scoring = new Scoring();
+        int[] allZeros = scoring.calculateScores(null);
+        assertArrayEquals(new int[3], allZeros);
+    }
+}
