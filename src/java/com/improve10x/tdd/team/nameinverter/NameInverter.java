ac17b6d212d4d18a5c64567d02c7739383721c3b
Vani <vani.pushpala@improve10x.com>
2023-07-22 11:41:12 +0530

    Added test case and logic for input single word

--- src/java/com/improve10x/tdd/team/nameinverter/NameInverter.java
+++ src/java/com/improve10x/tdd/team/nameinverter/NameInverter.java
@@ -5,6 +5,8 @@ public class NameInverter {
     public String nameInvert(String name) {
         if (name.equals("")){
             return "";
+        } else if (!name.contains(" ")) {
+            return name;
         }
         return null;
     }
--- src/tests/com/improve10x/tdd/team/nameinverter/NameInverterTest.java
+++ src/tests/com/improve10x/tdd/team/nameinverter/NameInverterTest.java
@@ -1,18 +1,28 @@
 package com.improve10x.tdd.team.nameinverter;
 
+import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
 
 import static org.junit.jupiter.api.Assertions.assertEquals;
 
 public class NameInverterTest {
+    NameInverter nameInverter;
+    @BeforeEach
+    public void setUp(){
+        nameInverter = new NameInverter();
+    }
     @Test
     public void nothing(){}
 
     @Test
     public void givenEmpty_returnsEmpty(){
-        NameInverter nameInverter = new NameInverter();
         String empty = nameInverter.nameInvert("");
         assertEquals("", empty);
     }
 
+    @Test
+    public void givenSingleWord_returnsSingleWord(){
+        String singleWord = nameInverter.nameInvert("Ammulu");
+        assertEquals("Ammulu", singleWord);
+    }
 }
