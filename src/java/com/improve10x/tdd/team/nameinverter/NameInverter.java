60a350bc0b828fac098e42653afa4aaf6b1767de
Vani <vani.pushpala@improve10x.com>
2023-07-22 11:33:41 +0530

    Added test case and logic for input empty

--- src/java/com/improve10x/tdd/team/nameinverter/NameInverter.java
+++ src/java/com/improve10x/tdd/team/nameinverter/NameInverter.java
@@ -1,36 +1,11 @@
 package com.improve10x.tdd.team.nameinverter;
 
-import java.util.ArrayList;
-import java.util.Arrays;
-
 public class NameInverter {
-    public String invertName(String name) {
-        if (name.equals("")) {
+
+    public String nameInvert(String name) {
+        if (name.equals("")){
             return "";
-        } else if (!name.trim().contains(" ")) {
-            return name.trim();
-        } else {
-            ArrayList<String> names = new ArrayList<>(Arrays.asList(name.trim().split("\\s+")));
-            for (int i = 0; i < names.size(); i++) {
-            }
-            removeHonorifics(names);
-            String postNominal = "";
-            for (int i = 2; i < names.size(); i++){
-                 postNominal += names.get(i)+ " ";
-            }
-            return (names.get(1) + ", " + names.get(0) + " " + postNominal).trim();
         }
-    }
-
-
-
-
-
-
-
-    private static void removeHonorifics(ArrayList<String> names) {
-        if (names.get(0).matches("mr.|mrs.|miss.")){
-            names.remove(0);
-        }
+        return null;
     }
 }
--- src/tests/com/improve10x/tdd/team/nameinverter/NameInverterTest.java
+++ src/tests/com/improve10x/tdd/team/nameinverter/NameInverterTest.java
@@ -1,79 +1,18 @@
 package com.improve10x.tdd.team.nameinverter;
 
-import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
 
 import static org.junit.jupiter.api.Assertions.assertEquals;
 
 public class NameInverterTest {
-    NameInverter nameInverter;
-
-    @BeforeEach
-    public void setUP(){
-        nameInverter = new NameInverter();
-    }
-
     @Test
     public void nothing(){}
 
     @Test
     public void givenEmpty_returnsEmpty(){
         NameInverter nameInverter = new NameInverter();
-        String empty = nameInverter.invertName("");
+        String empty = nameInverter.nameInvert("");
         assertEquals("", empty);
     }
 
-    @Test
-    public void givenSingleWord_returnsSingleWord(){
-        String singleWord = nameInverter.invertName("vani");
-        assertEquals("vani", singleWord);
-    }
-
-    @Test
-    public void givenSingleWordWithTrailingSpaces_returnsName(){
-        String name = nameInverter.invertName("mom  ");
-        assertEquals("mom", name);
-    }
-
-    @Test
-    public void givenSingleWordWithBeginningSpaces_returnsName(){
-        String name = nameInverter.invertName("  dad   ");
-        assertEquals("dad",name);
-    }
-
-    @Test
-    public void givenFirstLast_returnsLastFirst(){
-       String lastFirst = nameInverter.invertName("first last");
-       assertEquals("last, first", lastFirst);
-    }
-
-    @Test
-    public void givenHonorifics_removeHonorifics(){
-        String removeHonorifics = nameInverter.invertName("mrs. first last");
-        assertEquals("last, first", removeHonorifics);
-    }
-
-    @Test
-    public void givenMrHonorifics_removeMr(){
-        String removeMr = nameInverter.invertName("mr. first last");
-        assertEquals("last, first", removeMr);
-    }
-
-    @Test
-    public void givenPostNominal_StayAtEnd(){
-        String stayAtEnd = nameInverter.invertName("first last phd");
-        assertEquals("last, first phd", stayAtEnd);
-    }
-
-    @Test
-    public void givenMultiPostNominal_multiStayAtEnd(){
-        String multiStayAtEnd = nameInverter.invertName("first last phd. msc.");
-        assertEquals("last, first phd. msc.", multiStayAtEnd);
-    }
-
-    @Test
-    public void integrationTest(){
-        String invertName = nameInverter.invertName("mrs. first last phd. msc.");
-        assertEquals("last, first phd. msc.", invertName);
-    }
 }
