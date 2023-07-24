d6bdf8057cd704371e2bb845ca302910d34c1abc
Krishna <krishna.gangimeni@improve10x.com>
2023-07-24 17:37:41 +0530

    Added testcase and logic for input ZeroZero

---
+++ src/tests/com/improve10x/tdd/krishna/practice/euclideanalgorithm/EuclideanAlgorithmTest.java
@@ -0,0 +1,24 @@
+package com.improve10x.tdd.krishna.practice.euclideanalgorithm;
+
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
+
+import static org.junit.jupiter.api.Assertions.assertEquals;
+
+public class EuclideanAlgorithmTest {
+
+    EuclideanAlgorithm euclideanAlgorithm;
+
+    @BeforeEach
+    public void setEuclideanAlgorithm(){
+        euclideanAlgorithm = new EuclideanAlgorithm();
+    }
+    @Test
+    public void nothing(){
+    }
+    @Test
+    public void givenZeroZero_returnsZero(){
+        int euclidean = euclideanAlgorithm.findEuclidean(0, 0);
+        assertEquals(0, euclidean);
+    }
+}
