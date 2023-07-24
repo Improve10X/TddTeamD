d2064dd84b8b1f74042478db5a1e0c207beaa0bc
Krishna <krishna.gangimeni@improve10x.com>
2023-07-24 16:42:32 +0530

    Added testcase and logic for input Eight

--- src/tests/com/improve10x/tdd/krishna/practice/eulersphitest/EulersPhiTest.java
+++ src/tests/com/improve10x/tdd/krishna/practice/eulersphitest/EulersPhiTest.java
@@ -36,4 +36,9 @@ public class EulersPhiTest {
         int divisors = eulersPhi.findDivisors(3);
         assertEquals(2, divisors);
     }
+    @Test
+    public void givenEight_returnsFour(){
+        int divisors = eulersPhi.findDivisors(8);
+        assertEquals(4, divisors);
+    }
 }
