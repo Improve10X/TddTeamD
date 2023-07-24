809feafd915ac0713d4d39ea72dd50ee9de8df81
Supriya <supriya.singamsetty@improve10x.com>
2023-07-24 15:40:57 +0530

    Added TestCase And Logic For input ZeroZero

--- src/java/com/improve10x/tdd/supriya/practice/HowManyRectangles.java
+++
@@ -1,4 +0,0 @@
-package com.improve10x.tdd.supriya.practice;
-
-public class HowManyRectangles {
-}
---
+++ src/tests/com/improve10x/tdd/supriya/practice/CombinationTest.java
@@ -0,0 +1,27 @@
+package com.improve10x.tdd.supriya.practice;
+
+import org.junit.jupiter.api.Assertions;
+import org.junit.jupiter.api.Test;
+//Combinations
+//        Create a function that takes a variable number of arguments, each argument representing the number of items in a group. The function should return the number of permutations (combinations) of choices you would have if you selected one item from each group.
+//
+//        Examples
+//        combinations(2, 3) ➞ 6
+//
+//        combinations(3, 7, 4) ➞ 84
+//
+//        combinations(2, 3, 4, 5) ➞ 120
+
+public class CombinationTest {
+
+    @Test
+    public void nothing(){
+    }
+
+    @Test
+    public void givenZeroZero_returnsZero(){
+        Combination combination = new Combination();
+        int numbers = combination.findCombination(new int[]{0,0});
+        Assertions.assertEquals(0,numbers);
+    }
+}
