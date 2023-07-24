c6204fb4f00a27582cb0395136f4dd389d31adc2
Supriya <supriya.singamsetty@improve10x.com>
2023-07-24 16:48:46 +0530

    Added TestCase And Logic For input ZeroZero

---
+++ src/java/com/improve10x/tdd/supriya/practice/arethenumbersequal/AreTheNumbersEqual.java
@@ -0,0 +1,11 @@
+package com.improve10x.tdd.supriya.practice.arethenumbersequal;
+
+public class AreTheNumbersEqual {
+    public boolean findStatus(int a, int b) {
+        boolean result = false;
+        if (a == b){
+            result = true;
+        }
+        return result;
+    }
+}
