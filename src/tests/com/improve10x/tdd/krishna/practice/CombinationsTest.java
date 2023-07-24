18e1f388f7237e77ab4a9452c1c0ee32d3a89c30
Krishna <krishna.gangimeni@improve10x.com>
2023-07-24 15:39:41 +0530

    Added testcase and logic for input One Zero

--- src/tests/com/improve10x/tdd/krishna/practice/CombinationsTest.java
+++ src/tests/com/improve10x/tdd/krishna/practice/CombinationsTest.java
@@ -1,10 +1,18 @@
 package com.improve10x.tdd.krishna.practice;
 
+import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
 
 import static org.junit.jupiter.api.Assertions.assertEquals;
 
 public class CombinationsTest {
+
+    Combinations combinations;
+
+    @BeforeEach
+    public void setup(){
+        combinations = new Combinations();
+    }
 
     @Test
     public void nothing(){
@@ -16,9 +24,13 @@ public class CombinationsTest {
 //combinations(3, 7, 4) ➞ 84
 //combinations(2, 3, 4, 5) ➞ 120
     @Test
-    public void givenZeros_returnsAllZeros(){
-        Combinations combinations = new Combinations();
-        int allZeros = combinations.findCombination(new int[]{0, 0});
-        assertEquals(0, allZeros);
+    public void givenZeros_returnsZero(){
+        int zero = combinations.findCombination(new int[]{0, 0});
+        assertEquals(0, zero);
+    }
+    @Test
+    public void givenOneZero_returnsZero(){
+        int zero = combinations.findCombination(new int[]{1, 0});
+        assertEquals(0, zero);
     }
 }
