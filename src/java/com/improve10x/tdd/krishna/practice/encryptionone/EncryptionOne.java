fb65af0733a3e7b2433c82347fc48f75b48138fb
Krishna <krishna.gangimeni@improve10x.com>
2023-07-25 17:55:16 +0530

    Added testcase and logic for input empty and null and b

---
+++ src/java/com/improve10x/tdd/krishna/practice/encryptionone/EncryptionOne.java
@@ -0,0 +1,12 @@
+package com.improve10x.tdd.krishna.practice.encryptionone;
+
+public class EncryptionOne {
+    public String encrypt(String word) {
+        if(word != null) {
+            if (word.equals("b")) {
+                return word + "aca";
+            }
+        }
+        return  "aca";
+    }
+}
