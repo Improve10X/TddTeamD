db25f41704338e16b34647f65978d3f29febe18e
Krishna <krishna.gangimeni@improve10x.com>
2023-07-25 16:13:16 +0530

    Added testcase and logic for input lengths one and two

--- src/java/com/improve10x/tdd/krishna/practice/encryption/Encryption.java
+++ src/java/com/improve10x/tdd/krishna/practice/encryption/Encryption.java
@@ -2,9 +2,32 @@ package com.improve10x.tdd.krishna.practice.encryption;
 
 public class Encryption {
     public String findEncryption(String word) {
-        if(word.equals("b")){
-            return "b";
+        String result = "0";
+        for(int i = 0; i < word.length(); i++){
+            char c = word.charAt(i); //ab
+            if(c == 'a'){
+                result = "0";
+            }else if(c == 'b'){
+                result = " " + c;
+            }
         }
-        return "0";
+        return result;
+    }
+
+    public String encrypt(String word) {
+        if(word != null){
+            String reverseWord = "";
+            for(int i = word.length() - 1; i >= 0; i--){
+                reverseWord = reverseWord + word.charAt(i);
+            }
+            reverseWord = reverseWord.replace('a', '0');
+            reverseWord = reverseWord.replace('e', '1');
+            reverseWord = reverseWord.replace('i', '2');
+            reverseWord = reverseWord.replace('o', '3');
+            reverseWord = reverseWord.replace('u', '4');
+            return reverseWord + "aca";
+        }else{
+            return "aca";
+        }
     }
 }
