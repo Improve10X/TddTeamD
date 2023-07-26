5aea1a0e371c3c3cbb85b527121adf6aaf7cb633
Krishna <krishna.gangimeni@improve10x.com>
2023-07-26 15:33:13 +0530

    Added testcase and logic for input null

---
+++ src/java/com/improve10x/tdd/krishna/testproblems/Palindrome.java
@@ -0,0 +1,7 @@
+package com.improve10x.tdd.krishna.testproblems;
+
+public class Palindrome {
+    public boolean isPalindrome(String word) {
+        return true;
+    }
+}
---
+++ src/tests/com/improve10x/tdd/krishna/testproblems/PalindromeTest.java
@@ -0,0 +1,19 @@
+package com.improve10x.tdd.krishna.testproblems;
+
+import org.junit.jupiter.api.Test;
+
+import static org.junit.jupiter.api.Assertions.assertEquals;
+
+public class PalindromeTest {
+
+    @Test
+    public void nothing(){
+    }
+
+    @Test
+    public void givenNull_returnsTrue(){
+        Palindrome palindrome = new Palindrome();
+        boolean result = palindrome.isPalindrome(null);
+        assertEquals(true, result);
+    }
+}
