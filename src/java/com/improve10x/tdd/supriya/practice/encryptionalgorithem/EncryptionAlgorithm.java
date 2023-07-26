aa0bd8ba13a975fb59e7bfa31554995c84eaf383
Supriya <supriya.singamsetty@improve10x.com>
2023-07-26 10:38:40 +0530

    Added TestCase input doubleLetters

--- src/java/com/improve10x/tdd/supriya/practice/encryptionalgorithem/EncryptionAlgorithm.java
+++ src/java/com/improve10x/tdd/supriya/practice/encryptionalgorithem/EncryptionAlgorithm.java
@@ -3,17 +3,18 @@ package com.improve10x.tdd.supriya.practice.encryptionalgorithem;
 public class EncryptionAlgorithm {
     public String findEncrypt(String word) {
         if (word != null) {
-            if (word.equals("b")) {
-                return  word + "aca";
-            }else if (word.equals("a")){
-                return 0 +"aca";
-            }else if (word.equals("e")){
-                return 1 + "aca";
-            }else if (word.equals("i")){
-                return 2 + "aca";
+            String reverseWord = "";
+            for (int i = word.length() - 1; i >= 0; i--) {
+                reverseWord = reverseWord + word.charAt(i);
             }
+            reverseWord = reverseWord.replace('a', '0');
+            reverseWord = reverseWord.replace('e', '1');
+            reverseWord = reverseWord.replace('i', '2');
+            reverseWord = reverseWord.replace('o', '3');
+            reverseWord = reverseWord.replace('u', '4');
+            return reverseWord + "aca";
+        } else {
+            return  "aca";
         }
-        return "aca";
     }
-
 }
