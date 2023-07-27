d35193de16ab82d75e6d92bc57dd9e810eecab28
Krishna <krishna.gangimeni@improve10x.com>
2023-07-27 11:42:36 +0530

    Added testcase and logic for input Empty

--- src/java/com/improve10x/tdd/krishna/practice/countnoofspcaces/CountNoOfSpaces.java
+++ src/java/com/improve10x/tdd/krishna/practice/countnoofspcaces/CountNoOfSpaces.java
@@ -2,6 +2,11 @@ package com.improve10x.tdd.krishna.practice.countnoofspcaces;
 
 public class CountNoOfSpaces {
     public int count(String word) {
+        if(word != null){
+            if(word.contains(" ")){
+                return 1;
+            }
+        }
         return 0;
     }
 }
--- src/tests/com/improve10x/tdd/krishna/practice/countspacestest/CountNoOfSpacesTest.java
+++ src/tests/com/improve10x/tdd/krishna/practice/countspacestest/CountNoOfSpacesTest.java
@@ -1,11 +1,19 @@
 package com.improve10x.tdd.krishna.practice.countspacestest;
 
 import com.improve10x.tdd.krishna.practice.countnoofspcaces.CountNoOfSpaces;
+import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
 
 import static org.junit.jupiter.api.Assertions.assertEquals;
 
 public class CountNoOfSpacesTest {
+
+    CountNoOfSpaces countNoOfSpaces;
+
+    @BeforeEach
+    public void setup(){
+        countNoOfSpaces = new CountNoOfSpaces();
+    }
 
     @Test
     public void nothing(){
@@ -13,15 +21,19 @@ public class CountNoOfSpacesTest {
 
     @Test
     public void givenNull_returns0(){
-        CountNoOfSpaces countNoOfSpaces = new CountNoOfSpaces();
         int noOfSpaces = countNoOfSpaces.count(null);
         assertEquals(0, noOfSpaces);
     }
 
     @Test
     public void givenString_returns(){
-        CountNoOfSpaces countNoOfSpaces = new CountNoOfSpaces();
         int noOfSpaces = countNoOfSpaces.count("");
         assertEquals(0, noOfSpaces);
+    }
+
+    @Test
+    public void givenEmpty_returns1(){
+        int noOfSpaces = countNoOfSpaces.count(" ");
+        assertEquals(1, noOfSpaces);
     }
 }
