e24f55a7ecfcf26a6cefd8e13c8b234b4d0eca99
Supriya <supriya.singamsetty@improve10x.com>
2023-07-27 13:10:29 +0530

    Added TestCase input word

--- src/tests/com/improve10x/tdd/supriya/assingmentTest/countspacestest/CountSpacesTest.java
+++ src/tests/com/improve10x/tdd/supriya/assingmentTest/countspacestest/CountSpacesTest.java
@@ -23,9 +23,37 @@ public class CountSpacesTest {
     }
 
     @Test
+    public void givenabc_returnSpaces(){
+        CountSpaces countSpaces = new CountSpaces();
+        int countSpace = countSpaces.findSpaces("a b c");
+        Assertions.assertEquals(2, countSpace);
+    }
+
+    @Test
     public void givenName_returnZero(){
         CountSpaces countSpaces = new CountSpaces();
         int countSpace = countSpaces.findSpaces("Supriya");
         Assertions.assertEquals(0, countSpace);
+    }
+
+    @Test
+    public void givenimproveTenx_returnZero(){
+        CountSpaces countSpaces = new CountSpaces();
+        int countSpace = countSpaces.findSpaces("improve 10x");
+        Assertions.assertEquals(1, countSpace);
+    }
+
+    @Test
+    public void givenNameWithSpace_returnOne(){
+        CountSpaces countSpaces = new CountSpaces();
+        int countSpace = countSpaces.findSpaces("S Supriya");
+        Assertions.assertEquals(1,countSpace);
+    }
+
+    @Test
+    public void givenNameWithSpaces_returnTwo(){
+        CountSpaces countSpaces = new CountSpaces();
+        int countSpace = countSpaces.findSpaces("S Supriya Singamsetty");
+        Assertions.assertEquals(2,countSpace);
     }
 }
