0f5bad7e90d9aa6dc2993c26155b33ebee40c431
Supriya <supriya.singamsetty@improve10x.com>
2023-07-28 14:35:45 +0530

    Added TestCase input integrations

--- src/java/com/improve10x/tdd/supriya/practice/trnscribetomrna/TranscribeTomRNA.java
+++ src/java/com/improve10x/tdd/supriya/practice/trnscribetomrna/TranscribeTomRNA.java
@@ -2,13 +2,23 @@ package com.improve10x.tdd.supriya.practice.trnscribetomrna;
 
 public class TranscribeTomRNA {
     public String converted(String word) {
-        if (word == "A"){
-            return "U";
-        } else if (word == "T") {
-            return "A";
-        }else if (word == "G"){
-            return "C";
+        String result = "";
+        if ( word == null || word.equals("") ) {
+            return "";
         }
-        return "";
+        for (int i = 0; i < word.length(); i++) {
+            char c = word.charAt(i);
+            if (c == 'A') {
+                result += "U";
+            } else if (c == 'T') {
+                result += "A";
+            } else if (c == 'G') {
+                result += "C";
+            } else if (c == 'C') {
+                result +="G";
+            }
+        }
+        return result;
     }
 }
+
