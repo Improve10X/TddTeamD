4ceadbafe9e9c6de48a0d90aa580d1919a7e576a
Krishna <krishna.gangimeni@improve10x.com>
2023-07-28 11:19:21 +0530

    Added testcase and logic for input Empty

--- src/tests/com/improve10x/tdd/krishna/practice/practice/DNATomRNATest.java
+++ src/tests/com/improve10x/tdd/krishna/practice/practice/DNATomRNATest.java
@@ -1,25 +1,37 @@
 package com.improve10x.tdd.krishna.practice.practice;
 
+import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
 
 import static org.junit.jupiter.api.Assertions.assertEquals;
 
 public class DNATomRNATest {
 
+    DNATomRNA dnaTomRNA;
+
+    @BeforeEach
+    public void setup(){
+        dnaTomRNA = new DNATomRNA();
+    }
+
     @Test
     public void nothing(){
     }
     @Test
     public void givennull_returnsEmpty(){
-        DNATomRNA dnaTomRNA = new DNATomRNA();
         String converted = dnaTomRNA.convertTomRNA(null);
         assertEquals("", converted);
     }
 
     @Test
     public void givenString_returnsString(){
-        DNATomRNA dnaTomRNA = new DNATomRNA();
         String converted = dnaTomRNA.convertTomRNA("");
         assertEquals("", converted);
+    }
+
+    @Test
+    public void givenEmpty_returnsEmpty(){
+        String converted = dnaTomRNA.convertTomRNA(" ");
+        assertEquals(" ", converted);
     }
 }
