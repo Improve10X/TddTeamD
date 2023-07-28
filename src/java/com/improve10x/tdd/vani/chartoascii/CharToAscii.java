0f7d586a88f2edb5d0896846209184a51192f700
Vani <vani.pushpala@improve10x.com>
2023-07-28 16:01:26 +0530

    Added test case and logic for input char to ascii

--- src/java/com/improve10x/tdd/vani/chartoascii/CharToAscii.java
+++ src/java/com/improve10x/tdd/vani/chartoascii/CharToAscii.java
@@ -3,14 +3,21 @@ package com.improve10x.tdd.vani.chartoascii;
 public class CharToAscii {
     public int ctoa(String letter) {
         int result = 0;
+        if (letter.equals("SPACE")){
+            return 32;
+        }
         for (int i = 0; i < letter.length(); i++) {
             char c = letter.charAt(i);
             if (c == 'A') {
                 result = 65;
             } else if (c == 'B') {
                 result = 66;
-            } else {
-                result++;
+            } else if(c == 'C'){
+                result = 67;
+            }else if(c == '['){
+                result = 91;
+            }else if(c == '/'){
+                result = 92;
             }
         }
         return result;
