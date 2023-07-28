fedffaee4cc618b501681bbf2d1bdf28089cf464
Krishna <krishna.gangimeni@improve10x.com>
2023-07-28 15:56:44 +0530

    Added testcase and logic for input Zero

---
+++ src/java/com/improve10x/tdd/krishna/practice/practice/HowManyRectangles.java
@@ -0,0 +1,7 @@
+package com.improve10x.tdd.krishna.practice.practice;
+
+public class HowManyRectangles {
+    public int findNoOfRectangles(int num) {
+        return 0;
+    }
+}
---
+++ src/tests/com/improve10x/tdd/krishna/practice/practice/HowManyRectanglesTest.java
@@ -0,0 +1,20 @@
+package com.improve10x.tdd.krishna.practice.practice;
+
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
+
+import static org.junit.jupiter.api.Assertions.assertEquals;
+
+public class HowManyRectanglesTest {
+
+    @Test
+    public void nothing(){
+    }
+
+    @Test
+    public void givenZero_returnsZero(){
+        HowManyRectangles howManyRectangles = new HowManyRectangles();
+        int noOfRectangle = howManyRectangles.findNoOfRectangles(0);
+        assertEquals(0, noOfRectangle);
+    }
+}
