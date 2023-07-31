baca4f5697a0c2d4df5bb48b414ee607d4da9909
Krishna <krishna.gangimeni@improve10x.com>
2023-07-31 12:31:59 +0530

    Added testcase and logic for input Two

--- src/java/com/improve10x/tdd/krishna/practice/practice/HowManyPrimeNumbers.java
+++ src/java/com/improve10x/tdd/krishna/practice/practice/HowManyPrimeNumbers.java
@@ -1,7 +1,13 @@
 package com.improve10x.tdd.krishna.practice.practice;
 
 public class HowManyPrimeNumbers {
-    public int[] findPrimeNumbers(int number) {
-        return new int[0];
+    public int findPrimeNumbers(int number) {
+        int result = 0;
+        if (number >= 2) {
+            if(number % 1 == 0 && (number % number) == 0){
+                result = 1;
+            }
+        }
+        return result;
     }
 }
