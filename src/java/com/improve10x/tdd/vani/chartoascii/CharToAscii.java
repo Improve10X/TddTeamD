96efbbdd49326f85d00deb9adf0fd93c3db9929e
Vani <vani.pushpala@improve10x.com>
2023-07-31 15:11:37 +0530

    Added test case and logic for input null and space

--- src/java/com/improve10x/tdd/vani/chartoascii/CharToAscii.java
+++ src/java/com/improve10x/tdd/vani/chartoascii/CharToAscii.java
@@ -1,25 +1,34 @@
 package com.improve10x.tdd.vani.chartoascii;
 
 public class CharToAscii {
-    public int ctoa(String letter) {
+    public int ctoa(Character letter) {
         int result = 0;
-        if (letter.equals("SPACE")){
+       if (letter == null){
+           return 0;
+       }
+        if (letter.equals(' ')) {
             return 32;
-        }
-        for (int i = 0; i < letter.length(); i++) {
-            char c = letter.charAt(i);
-            if (c == 'A') {
-                result = 65;
-            } else if (c == 'B') {
-                result = 66;
-            } else if(c == 'C'){
-                result = 67;
-            }else if(c == '['){
-                result = 91;
-            }else if(c == '/'){
-                result = 92;
-            }
         }
         return result;
     }
 }
+//        int result = 0;
+//        if (letter.equals("SPACE")){
+//            return 32;
+//        }
+//        for (int i = 0; i < letter.length(); i++) {
+//            char c = letter.charAt(i);
+//            if (c == 'A') {
+//                result = 65;
+//            } else if (c == 'B') {
+//                result = 66;
+//            } else if(c == 'C'){
+//                result = 67;
+//            }else if(c == '['){
+//                result = 91;
+//            }else if(c == '\u0000'){
+//                result = 92;
+//            }
+//        }
+//        return result;
+//    }
