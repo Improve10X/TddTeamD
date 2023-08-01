bb34bb1d64b41307154da33eaaa04ace6c062bc1
Vani <vani.pushpala@improve10x.com>
2023-08-01 14:20:18 +0530

    Added test case and logic for input find the missing number

--- src/tests/com/improve10x/tdd/vani/findthemissingnumber/FindTheMissingNumberTest.java
+++ src/tests/com/improve10x/tdd/vani/findthemissingnumber/FindTheMissingNumberTest.java
@@ -1,6 +1,7 @@
 package com.improve10x.tdd.vani.findthemissingnumber;
 
 import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Disabled;
 import org.junit.jupiter.api.Test;
 
 import static org.junit.jupiter.api.Assertions.assertEquals;
@@ -12,28 +13,47 @@ import static org.junit.jupiter.api.Assertions.assertEquals;
 //        missingNum([10, 5, 1, 2, 4, 6, 8, 3, 9]) ➞ 7
 
 public class FindTheMissingNumberTest {
-    FindTheMissingNumber findTheMissingNumber;
+    MissingNumber missingNumber;
     @BeforeEach
     public void setUp(){
-        findTheMissingNumber = new FindTheMissingNumber();
+        missingNumber = new MissingNumber();
     }
     @Test
     public void nothing(){}
     @Test
-    public void givenNull_returns0(){
-        int number = findTheMissingNumber.missingNumber(null);
-        assertEquals(0,number);
+    public void givenNull_returnsMinusOne(){
+        int number = missingNumber.findMissingNumber(null);
+        assertEquals(-1,number);
     }
     @Test
-    public void given0_returns0(){
-        int number = findTheMissingNumber.missingNumber(new int[]{0});
-        assertEquals(0,number);
+    public void givenOneThree_returnsMinusOne(){
+        int number = missingNumber.findMissingNumber(new int[]{1,3});
+        assertEquals(-1,number);
+    }
+
+    @Test
+    public void givenNumbers1To10_returnsMinusOne(){
+        int number = missingNumber.findMissingNumber(new int[]{1,2,3,4,5,6,7,8,9,10});
+        assertEquals(-1,number);
     }
     @Test
-    public void givenZer0Two_returns1(){
-        int number = findTheMissingNumber.missingNumber(new int[]{0,2});
-        assertEquals(1,number);
+    public void givenNumbers_returnsMinus1(){
+        int number = missingNumber.findMissingNumber(new int[]{1,2,3,5});
+        assertEquals(-1,number);
     }
 
-
+    @Test
+    public void givenIntegers_returnsMinus1(){
+        int number = missingNumber.findMissingNumber(new int[]{1,3,5});
+        assertEquals(-1,number);
+    }
+    @Test
+    public void integrationTest(){
+        int number = missingNumber.findMissingNumber(new int[]{1,2,3,4,6,7,8,9,10});
+        assertEquals(5,number);
+        int number1 = missingNumber.findMissingNumber(new int[]{7, 2, 3, 6, 5, 9, 1, 4, 8});
+        assertEquals(10,number1);
+        int number2 = missingNumber.findMissingNumber(new int[]{10, 5, 1, 2, 4, 6, 8, 3, 9});
+        assertEquals(7,number2);
+    }
 }
