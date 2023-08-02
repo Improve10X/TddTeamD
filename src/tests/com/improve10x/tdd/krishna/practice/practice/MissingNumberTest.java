a811f2ec02a142db642ae6946168f1c36da2dcd8
Krishna <krishna.gangimeni@improve10x.com>
2023-08-02 11:45:05 +0530

    Added testcase for input integration

--- src/tests/com/improve10x/tdd/krishna/practice/practice/MissingNumberTest.java
+++ src/tests/com/improve10x/tdd/krishna/practice/practice/MissingNumberTest.java
@@ -47,4 +47,14 @@ public class MissingNumberTest {
         int result = MissingNumber.findMissingNumber(new int[]{1, 3, 4, 5, 6, 7, 8, 9, 10});
         assertEquals(2, result);
     }
+
+    @Test
+    public void integration(){
+        int result = MissingNumber.findMissingNumber(new int[]{1, 2, 3, 4, 6, 7, 8, 9, 10});
+        assertEquals(5, result);
+        int result1 = MissingNumber.findMissingNumber(new int[]{7, 2, 3, 6, 5, 9, 1, 4, 8});
+        assertEquals(10, result1);
+        int result2 = MissingNumber.findMissingNumber(new int[]{10, 5, 1, 2, 4, 6, 8, 3, 9});
+        assertEquals(7, result2);
+    }
 }
