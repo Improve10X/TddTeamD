22ce2f194a927d8c6ecd28ff3fb7a1feb6300685
Krishna <krishna.gangimeni@improve10x.com>
2023-08-10 16:22:31 +0530

    Added testcase and logic for input 2

--- src/java/com/improve10x/tdd/krishna/test1problems/MajorityElement.java
+++ src/java/com/improve10x/tdd/krishna/test1problems/MajorityElement.java
@@ -3,8 +3,12 @@ package com.improve10x.tdd.krishna.test1problems;
 public class MajorityElement {
     public int findMajorityElement(int[] numbers) {
         int n = numbers.length;
-        if(n == 1 && (n / 2) < 1){
-            return 1;
+        if(n == 1){
+            if((n / 2) < 1 && numbers[0] == 1){
+                return 1;
+            }else if((n / 2) < 2){
+                return 2;
+            }
         }
         return -1;
     }
--- src/tests/com/improve10x/tdd/krishna/test1problems/findmajorityelement/MajorityElementTest.java
+++ src/tests/com/improve10x/tdd/krishna/test1problems/findmajorityelement/MajorityElementTest.java
@@ -1,11 +1,18 @@
 package com.improve10x.tdd.krishna.test1problems.findmajorityelement;
 
 import com.improve10x.tdd.krishna.test1problems.MajorityElement;
+import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
 
 import static org.junit.jupiter.api.Assertions.assertEquals;
 
 public class MajorityElementTest {
+    MajorityElement majorityElement;
+
+    @BeforeEach
+    public void setup(){
+        majorityElement = new MajorityElement();
+    }
 
     @Test
     public void nothing(){
@@ -13,15 +20,19 @@ public class MajorityElementTest {
 
     @Test
     public void given0_returnsMinusOne(){
-        MajorityElement majorityElement = new MajorityElement();
         int result = majorityElement.findMajorityElement(new int[0]);
         assertEquals(-1, result);
     }
 
     @Test
     public void given1_returnsOne(){
-        MajorityElement majorityElement = new MajorityElement();
         int result = majorityElement.findMajorityElement(new int[]{1});
         assertEquals(1, result);
+    }
+
+    @Test
+    public void given2_returns2(){
+        int result = majorityElement.findMajorityElement(new int[]{2});
+        assertEquals(2, result);
     }
 }
