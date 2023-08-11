8f0d49a92a03ad2b2dffdf674e5cde7a4330c12f
Krishna <krishna.gangimeni@improve10x.com>
2023-08-11 16:46:42 +0530

    Added testcase and logic for input null in LargestNumberTest

--- src/tests/com/improve10x/tdd/krishna/test2problems/largestnumbertest/LargestNumberTest.java
+++ src/tests/com/improve10x/tdd/krishna/test2problems/largestnumbertest/LargestNumberTest.java
@@ -1,10 +1,20 @@
 package com.improve10x.tdd.krishna.test2problems.largestnumbertest;
 
+import com.improve10x.tdd.krishna.test2problems.largestnumber.LargestNumber;
 import org.junit.jupiter.api.Test;
+
+import static org.junit.jupiter.api.Assertions.assertEquals;
 
 public class LargestNumberTest {
 
     @Test
     public void nothing(){
+    }
+
+    @Test
+    public void givenNull_returnsZero(){
+        LargestNumber largestNumber = new LargestNumber();
+        int largestNum = largestNumber.findLargestNumber(null);
+        assertEquals(0, largestNum);
     }
 }
