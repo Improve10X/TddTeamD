549bdbbfd008e98df9e2d1830365ad2da80c3569
Krishna <krishna.gangimeni@improve10x.com>
2023-08-11 13:09:43 +0530

    Some changes

--- src/tests/com/improve10x/tdd/krishna/test1problems/leadersinanarraytest/LeadersInAnArrayTest.java
+++ src/tests/com/improve10x/tdd/krishna/test1problems/leadersinanarraytest/LeadersInAnArrayTest.java
@@ -1,11 +1,20 @@
 package com.improve10x.tdd.krishna.test1problems.leadersinanarraytest;
 
 import com.improve10x.tdd.krishna.test1problems.leadersinanarray.LeadersInAnArray;
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Disabled;
 import org.junit.jupiter.api.Test;
 
 import static org.junit.jupiter.api.Assertions.assertArrayEquals;
 
 public class LeadersInAnArrayTest {
+
+    LeadersInAnArray leadersInAnArray;
+
+    @BeforeEach
+    public void setup(){
+        leadersInAnArray = new LeadersInAnArray();
+    }
 
     @Test
     public void nothing(){
@@ -13,43 +22,44 @@ public class LeadersInAnArrayTest {
 
     @Test
     public void givenZero_returnsZero(){
-        LeadersInAnArray leadersInAnArray = new LeadersInAnArray();
         int[] leaders = leadersInAnArray.findLeadersInAnArray(new int[] {0});
         assertArrayEquals(new int[]{0}, leaders);
     }
 
     @Test
     public void givenOne_returnsOne(){
-        LeadersInAnArray leadersInAnArray = new LeadersInAnArray();
         int[] leaders = leadersInAnArray.findLeadersInAnArray(new int[] {1});
         assertArrayEquals(new int[]{1}, leaders);
     }
 
     @Test
     public void givenMinusOne_returnsMinusOne(){
-        LeadersInAnArray leadersInAnArray = new LeadersInAnArray();
         int[] leaders = leadersInAnArray.findLeadersInAnArray(new int[] {-1});
         assertArrayEquals(new int[]{-1}, leaders);
     }
 
     @Test
     public void givenZeroZero_returnsZero(){
-        LeadersInAnArray leadersInAnArray = new LeadersInAnArray();
         int[] leaders = leadersInAnArray.findLeadersInAnArray(new int[] {0 , 0});
         assertArrayEquals(new int[]{0}, leaders);
     }
 
     @Test
     public void givenZeroOne_returnsOne(){
-        LeadersInAnArray leadersInAnArray = new LeadersInAnArray();
         int[] leaders = leadersInAnArray.findLeadersInAnArray(new int[] {0, 1});
         assertArrayEquals(new int[]{1}, leaders);
     }
 
     @Test
     public void givenOneTwo_returnsTwo(){
-        LeadersInAnArray leadersInAnArray = new LeadersInAnArray();
         int[] leaders = leadersInAnArray.findLeadersInAnArray(new int[] {1, 2});
         assertArrayEquals(new int[]{2}, leaders);
+    }
+
+    @Disabled
+    @Test
+    public void givenTwoOne_returnsTwoOne(){
+        int[] leaders = leadersInAnArray.findLeadersInAnArray(new int[] {2, 1});
+        assertArrayEquals(new int[]{2, 1}, leaders);
     }
 }
