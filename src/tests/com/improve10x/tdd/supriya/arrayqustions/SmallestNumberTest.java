5a4e95d90d7c706cba4859cea524ef53cd575710
Supriya <supriya.singamsetty@improve10x.com>
2023-08-11 16:19:12 +0530

    Added test case for given null

---
+++ src/tests/com/improve10x/tdd/supriya/arrayqustions/SmallestNumberTest.java
@@ -0,0 +1,19 @@
+package com.improve10x.tdd.supriya.arrayqustions;
+
+import com.improve10x.tdd.supriya.arrayquestions.smallestNumber.SmallestNumber;
+import org.junit.jupiter.api.Assertions;
+import org.junit.jupiter.api.Test;
+
+public class SmallestNumberTest {
+
+    @Test
+    public void nothing(){}
+
+    @Test
+    public void givenNull_returnMinus0ne(){
+        SmallestNumber smallestNumber = new SmallestNumber();
+        int number = smallestNumber.findSmallestNumber(null);
+        Assertions.assertEquals(0, number);
+    }
+
+}
