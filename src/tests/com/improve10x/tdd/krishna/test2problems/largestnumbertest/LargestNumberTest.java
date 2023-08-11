8f617ceb32b06d1928d4a75c4a3989daafb8ab2f
Krishna <krishna.gangimeni@improve10x.com>
2023-08-11 16:58:23 +0530

    Added testcase for input Zero in LargestNumberTest

--- src/tests/com/improve10x/tdd/krishna/test2problems/largestnumbertest/LargestNumberTest.java
+++ src/tests/com/improve10x/tdd/krishna/test2problems/largestnumbertest/LargestNumberTest.java
@@ -1,11 +1,19 @@
 package com.improve10x.tdd.krishna.test2problems.largestnumbertest;
 
 import com.improve10x.tdd.krishna.test2problems.largestnumber.LargestNumber;
+import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
 
 import static org.junit.jupiter.api.Assertions.assertEquals;
 
 public class LargestNumberTest {
+
+    LargestNumber largestNumber;
+
+    @BeforeEach
+    public void setup(){
+        largestNumber = new LargestNumber();
+    }
 
     @Test
     public void nothing(){
@@ -13,15 +21,19 @@ public class LargestNumberTest {
 
     @Test
     public void givenNull_returnsZero(){
-        LargestNumber largestNumber = new LargestNumber();
         int largestNum = largestNumber.findLargestNumber(null);
         assertEquals(0, largestNum);
     }
 
     @Test
     public void givenEmpty_returnsZero(){
-        LargestNumber largestNumber = new LargestNumber();
         int largestNum = largestNumber.findLargestNumber(new int[]{});
+        assertEquals(0, largestNum);
+    }
+
+    @Test
+    public void givenZero_returnsZero(){
+        int largestNum = largestNumber.findLargestNumber(new int[]{0});
         assertEquals(0, largestNum);
     }
 }
