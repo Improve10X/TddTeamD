c8358b6c68644d006a036dda87ad08fdd099d6b4
Supriya <supriya.singamsetty@improve10x.com>
2023-08-14 11:23:32 +0530

    Added test case for given null

---
+++ src/java/com/improve10x/tdd/supriya/arrayquestions/missingnumber/MissingNumber1.java
@@ -0,0 +1,7 @@
+package com.improve10x.tdd.supriya.arrayquestions.missingnumber;
+
+public class MissingNumber1 {
+    public int findMissingNumber(int[] numbers) {
+        return -1;
+    }
+}
--- src/tests/com/improve10x/tdd/supriya/arrayqustions/SmallestNumberTest.java
+++ src/tests/com/improve10x/tdd/supriya/arrayqustions/SmallestNumberTest.java
@@ -2,66 +2,105 @@ package com.improve10x.tdd.supriya.arrayqustions;
 
 import com.improve10x.tdd.supriya.arrayquestions.smallestNumber.SmallestNumber;
 import org.junit.jupiter.api.Assertions;
+import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Disabled;
 import org.junit.jupiter.api.Test;
 
+import static org.junit.jupiter.api.Assertions.assertEquals;
+
 public class SmallestNumberTest {
+    SmallestNumber smallestNumber;
+
+    @BeforeEach
+    public void setUp() {
+        smallestNumber = new SmallestNumber();
+    }
 
     @Test
-    public void nothing(){}
+    public void nothing() {
+    }
 
     @Test
-    public void givenNull_return0(){
-        SmallestNumber smallestNumber = new SmallestNumber();
+    public void givenNull_return0() {
         int number = smallestNumber.findSmallestNumber(null);
-        Assertions.assertEquals(0, number);
+        assertEquals(0, number);
     }
 
     @Test
-    public void givenEmpty_return0(){
-        SmallestNumber smallestNumber = new SmallestNumber();
+    public void givenEmpty_return0() {
         int number = smallestNumber.findSmallestNumber(new int[]{});
-        Assertions.assertEquals(0, number);
+        assertEquals(0, number);
     }
 
     @Test
-    public void given0_return0(){
-        SmallestNumber smallestNumber = new SmallestNumber();
+    public void given0_return0() {
         int number = smallestNumber.findSmallestNumber(new int[]{0});
-        Assertions.assertEquals(0, number);
+        assertEquals(0, number);
     }
 
     @Test
-    public void given10_return0(){
-        SmallestNumber smallestNumber = new SmallestNumber();
-        int number = smallestNumber.findSmallestNumber(new int[]{0});
-        Assertions.assertEquals(0, number);
+    public void given10_return0() {
+        int number = smallestNumber.findSmallestNumber(new int[]{1, 0});
+        assertEquals(0, number);
     }
 
     @Test
-    public void given21_return1(){
-        SmallestNumber smallestNumber = new SmallestNumber();
-        int number = smallestNumber.findSmallestNumber(new int[]{2,1});
-        Assertions.assertEquals(1, number);
-    }
-    @Test
-    public void given35_return3(){
-        SmallestNumber smallestNumber = new SmallestNumber();
-        int number = smallestNumber.findSmallestNumber(new int[]{3,5});
-        Assertions.assertEquals(3, number);
+    public void given21_return1() {
+        int number = smallestNumber.findSmallestNumber(new int[]{2, 1});
+        assertEquals(1, number);
     }
 
     @Test
-    public void given231_return1(){
-        SmallestNumber smallestNumber = new SmallestNumber();
-        int number = smallestNumber.findSmallestNumber(new int[]{2,3,1});
-        Assertions.assertEquals(1, number);
+    public void given35_return3() {
+        int number = smallestNumber.findSmallestNumber(new int[]{3, 5});
+        assertEquals(3, number);
     }
 
     @Test
-    public void given4532_return1(){
-        SmallestNumber smallestNumber = new SmallestNumber();
-        int number = smallestNumber.findSmallestNumber(new int[]{4,5,3,2});
-        Assertions.assertEquals(2, number);
+    public void given231_return1() {
+        int number = smallestNumber.findSmallestNumber(new int[]{2, 3, 1});
+        assertEquals(1, number);
+    }
+
+    @Test
+    public void given123_return1() {
+        int number = smallestNumber.findSmallestNumber(new int[]{1, 2, 3});
+        assertEquals(1, number);
+    }
+
+    @Test
+    public void given210_return0() {
+        int number = smallestNumber.findSmallestNumber(new int[]{2, 1, 0});
+        assertEquals(0, number);
+    }
+
+    @Test
+    public void given1234_return1() {
+        int number = smallestNumber.findSmallestNumber(new int[]{1, 2, 3, 4});
+        assertEquals(1, number);
+    }
+
+    @Test
+    public void given2134_return1() {
+        int number = smallestNumber.findSmallestNumber(new int[]{2, 1, 3, 4});
+        assertEquals(1, number);
+    }
+
+    @Test
+    public void given2314_return1() {
+        int number = smallestNumber.findSmallestNumber(new int[]{2, 3, 1, 4});
+        assertEquals(1, number);
+    }
+
+    @Test
+    public void given2341_return1() {
+        int number = smallestNumber.findSmallestNumber(new int[]{2, 3, 4, 1});
+        assertEquals(1, number);
+    }
+
+    @Test
+    public void given4635_return3() {
+        int number = smallestNumber.findSmallestNumber(new int[]{4,6,3,5});
+        assertEquals(3, number);
     }
 }
