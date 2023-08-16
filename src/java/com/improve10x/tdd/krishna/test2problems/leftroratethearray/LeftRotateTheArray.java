f554d995d30db56df6df363765c0cc1f839661cc
Krishna <krishna.gangimeni@improve10x.com>
2023-08-16 17:23:29 +0530

    Added testcase and logic for input null in LeftRotateTheArrayTest

---
+++ src/java/com/improve10x/tdd/krishna/test2problems/leftroratethearray/LeftRotateTheArray.java
@@ -0,0 +1,7 @@
+package com.improve10x.tdd.krishna.test2problems.leftroratethearray;
+
+public class LeftRotateTheArray {
+    public int[] findLeftRotateArray(int[] numbers) {
+        return new int[]{};
+    }
+}
---
+++ src/tests/com/improve10x/tdd/krishna/test2problems/leftrotatethearraytest/LeftRotateTheArrayTest.java
@@ -0,0 +1,20 @@
+package com.improve10x.tdd.krishna.test2problems.leftrotatethearraytest;
+
+import com.improve10x.tdd.krishna.test2problems.leftroratethearray.LeftRotateTheArray;
+import org.junit.jupiter.api.Test;
+
+import static org.junit.jupiter.api.Assertions.assertArrayEquals;
+
+public class LeftRotateTheArrayTest {
+
+    @Test
+    public void nothing(){
+    }
+
+    @Test
+    public void givenNull_returnsEmpty(){
+        LeftRotateTheArray leftRotateTheArray = new LeftRotateTheArray();
+        int[] result = leftRotateTheArray.findLeftRotateArray(null);
+        assertArrayEquals(new int[]{}, result);
+    }
+}
