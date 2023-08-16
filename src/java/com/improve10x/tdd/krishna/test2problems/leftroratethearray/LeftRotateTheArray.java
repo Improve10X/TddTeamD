96b37d7b969b9fce3619fd70fa9f72736a33415f
Krishna <krishna.gangimeni@improve10x.com>
2023-08-16 20:31:19 +0530

    Added testcase and logic for input 123 in LeftRotateTheArrayTest

--- src/java/com/improve10x/tdd/krishna/test2problems/leftroratethearray/LeftRotateTheArray.java
+++ src/java/com/improve10x/tdd/krishna/test2problems/leftroratethearray/LeftRotateTheArray.java
@@ -2,22 +2,22 @@ package com.improve10x.tdd.krishna.test2problems.leftroratethearray;
 
 public class LeftRotateTheArray {
     public int[] findLeftRotateArray(int[] numbers) {
-        if(numbers != null){
-            if(numbers.length == 1){
-                return new int[] {numbers[0]};
+        if (numbers != null) {
+            if (numbers.length == 1) {
+                return new int[]{numbers[0]};
             }
-            if(numbers.length == 2){
+            if (numbers.length == 2) {
                 int temp = numbers[0];
                 numbers[0] = numbers[1];
                 numbers[1] = temp;
-                return new int[] {numbers[0],numbers[1]};
+                return new int[]{numbers[0], numbers[1]};
             }
-            if(numbers.length == 3){
+            if (numbers.length == 3) {
                 int temp = numbers[0];
                 numbers[0] = numbers[1];
                 numbers[1] = numbers[2];
                 numbers[2] = temp;
-                return new int[] {numbers[0], numbers[1], numbers[2]};
+                return new int[]{numbers[0], numbers[1], numbers[2]};
             }
         }
         return new int[]{};
