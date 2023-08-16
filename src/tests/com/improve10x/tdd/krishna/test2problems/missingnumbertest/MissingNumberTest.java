3aa54a75a405baae6c20cd432254457b725bbfc9
Krishna <krishna.gangimeni@improve10x.com>
2023-08-16 15:47:00 +0530

    Added testcase for input OneTwoThreeFourFiveSixEight MissingNumberTest

--- src/tests/com/improve10x/tdd/krishna/test2problems/missingnumbertest/MissingNumberTest.java
+++ src/tests/com/improve10x/tdd/krishna/test2problems/missingnumbertest/MissingNumberTest.java
@@ -80,8 +80,8 @@ public class MissingNumberTest {
     }
 
     @Test
-    public void given1234568_returnsThree(){
-        int missingNum = missingNumber.findMissingNumber(new int[]{1, 2, 4, 5});
-        assertEquals(3, missingNum);
+    public void given1234568_returnsSeven(){
+        int missingNum = missingNumber.findMissingNumber(new int[]{1, 2, 3, 4, 5, 6, 8});
+        assertEquals(7, missingNum);
     }
 }
