485cbc471cd5f4ec6c5cb2c6e15909c645914ccb
Krishna <krishna.gangimeni@improve10x.com>
2023-08-16 11:38:39 +0530

    Added testcase and logic for input null in MissingNumberTest

---
+++ src/tests/com/improve10x/tdd/krishna/test2problems/missingnumbertest/MissingNumberTest.java
@@ -0,0 +1,20 @@
+package com.improve10x.tdd.krishna.test2problems.missingnumbertest;
+
+import com.improve10x.tdd.krishna.test2problems.missingnumber.MissingNumber;
+import org.junit.jupiter.api.Test;
+
+import static org.junit.jupiter.api.Assertions.assertEquals;
+
+public class MissingNumberTest {
+
+    @Test
+    public void nothing(){
+    }
+
+    @Test
+    public void givenNull_returnsMinusOne(){
+        MissingNumber missingNumber = new MissingNumber();
+        int missingNum = missingNumber.findMissingNumber(null);
+        assertEquals(-1, missingNum);
+    }
+}
