6b6b2bbe3896b8b7b0dd87cdb96e12e5a2630882
Krishna <krishna.gangimeni@improve10x.com>
2023-08-16 17:34:25 +0530

    Added testcase and logic for input Zero in LeftRotateTheArrayTest

--- src/java/com/improve10x/tdd/krishna/test2problems/leftroratethearray/LeftRotateTheArray.java
+++ src/java/com/improve10x/tdd/krishna/test2problems/leftroratethearray/LeftRotateTheArray.java
@@ -2,6 +2,9 @@ package com.improve10x.tdd.krishna.test2problems.leftroratethearray;
 
 public class LeftRotateTheArray {
     public int[] findLeftRotateArray(int[] numbers) {
+        if(numbers != null && numbers.length == 1){
+            return new int[] {0};
+        }
         return new int[]{};
     }
 }
--- src/tests/com/improve10x/tdd/krishna/test2problems/leftrotatethearraytest/LeftRotateTheArrayTest.java
+++ src/tests/com/improve10x/tdd/krishna/test2problems/leftrotatethearraytest/LeftRotateTheArrayTest.java
@@ -1,11 +1,19 @@
 package com.improve10x.tdd.krishna.test2problems.leftrotatethearraytest;
 
 import com.improve10x.tdd.krishna.test2problems.leftroratethearray.LeftRotateTheArray;
+import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
 
 import static org.junit.jupiter.api.Assertions.assertArrayEquals;
 
 public class LeftRotateTheArrayTest {
+
+    private LeftRotateTheArray leftRotateTheArray;
+
+    @BeforeEach
+    public void setup(){
+        leftRotateTheArray = new LeftRotateTheArray();
+    }
 
     @Test
     public void nothing(){
@@ -13,15 +21,19 @@ public class LeftRotateTheArrayTest {
 
     @Test
     public void givenNull_returnsEmpty(){
-        LeftRotateTheArray leftRotateTheArray = new LeftRotateTheArray();
         int[] result = leftRotateTheArray.findLeftRotateArray(null);
         assertArrayEquals(new int[]{}, result);
     }
 
     @Test
     public void givenEmpty_returnsEmpty(){
-        LeftRotateTheArray leftRotateTheArray = new LeftRotateTheArray();
         int[] result = leftRotateTheArray.findLeftRotateArray(new int[]{});
         assertArrayEquals(new int[]{}, result);
+    }
+
+    @Test
+    public void givenZero_returnsZero(){
+        int[] result = leftRotateTheArray.findLeftRotateArray(new int[]{0});
+        assertArrayEquals(new int[]{0}, result);
     }
 }
