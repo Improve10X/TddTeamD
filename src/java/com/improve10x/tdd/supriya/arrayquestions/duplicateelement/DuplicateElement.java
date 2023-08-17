bd1123d05ba228dc525797eca7f3dbb592cf3408
Supriya <supriya.singamsetty@improve10x.com>
2023-08-17 12:27:02 +0530

    Added test case for given 12

--- src/java/com/improve10x/tdd/supriya/arrayquestions/duplicateelement/DuplicateElement.java
+++ src/java/com/improve10x/tdd/supriya/arrayquestions/duplicateelement/DuplicateElement.java
@@ -2,11 +2,18 @@ package com.improve10x.tdd.supriya.arrayquestions.duplicateelement;
 
 public class DuplicateElement {
     public int[] find(int[] numbers) {
-        if (numbers != null&& numbers.length!=0){
-            if (numbers[0] == numbers[0]){
-                return new int[] {numbers[0]};
+        if (numbers != null&& numbers.length!=0) {
+            if (numbers.length == 1 && numbers[0] == numbers[0]) {
+                return new int[]{numbers[0]};
+            }
+            if (numbers.length == 2) {
+                if (numbers[0] == numbers[1]) {
+                    return new int[]{numbers[0]};
+                }
+                return new int[]{numbers[0], numbers[1]};
             }
         }
+        
         return new int[]{};
     }
 }
