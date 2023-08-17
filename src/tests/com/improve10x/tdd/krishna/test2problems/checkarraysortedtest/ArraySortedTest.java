d3ee02915443e96699e1ff5e2265be4c0c47d5f4
Krishna <krishna.gangimeni@improve10x.com>
2023-08-17 12:02:21 +0530

    Added testcase for input 01 in ArraySortedTest

--- src/tests/com/improve10x/tdd/krishna/test2problems/checkarraysortedtest/ArraySortedTest.java
+++ src/tests/com/improve10x/tdd/krishna/test2problems/checkarraysortedtest/ArraySortedTest.java
@@ -1,11 +1,19 @@
 package com.improve10x.tdd.krishna.test2problems.checkarraysortedtest;
 
 import com.improve10x.tdd.krishna.test2problems.checkarraysorted.ArraySorted;
+import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
 
 import static org.junit.jupiter.api.Assertions.assertTrue;
 
 public class ArraySortedTest {
+
+    private   ArraySorted arraySorted;
+
+    @BeforeEach
+    public void setup(){
+        arraySorted = new ArraySorted();
+    }
 
     @Test
     public void nothing(){
@@ -13,29 +21,31 @@ public class ArraySortedTest {
 
     @Test
     public void givenNull_returnsTrue(){
-        ArraySorted arraySorted = new ArraySorted();
         boolean result = arraySorted.isSortedArray(null);
         assertTrue(result);
     }
 
     @Test
     public void givenEmpty_returnsTrue(){
-        ArraySorted arraySorted = new ArraySorted();
         boolean result = arraySorted.isSortedArray(new int[] {});
         assertTrue(result);
     }
 
     @Test
     public void given0_returnsTrue(){
-        ArraySorted arraySorted = new ArraySorted();
         boolean result = arraySorted.isSortedArray(new int[]{0});
         assertTrue(result);
     }
 
     @Test
     public void given1_returnsTrue(){
-        ArraySorted arraySorted = new ArraySorted();
         boolean result = arraySorted.isSortedArray(new int[]{1});
+        assertTrue(result);
+    }
+
+    @Test
+    public void given01_returnsTrue(){
+        boolean result = arraySorted.isSortedArray(new int[]{0, 1});
         assertTrue(result);
     }
 }
