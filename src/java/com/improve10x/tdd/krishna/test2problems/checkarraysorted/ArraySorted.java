75535ea3f72b2f343ae57ae5b68c28423b7853ea
Krishna <krishna.gangimeni@improve10x.com>
2023-08-17 11:55:23 +0530

    Added testcase and logic for input null in ArraySortedTest

---
+++ src/java/com/improve10x/tdd/krishna/test2problems/checkarraysorted/ArraySorted.java
@@ -0,0 +1,7 @@
+package com.improve10x.tdd.krishna.test2problems.checkarraysorted;
+
+public class ArraySorted {
+    public boolean isSortedArray(int[] numbers) {
+        return true;
+    }
+}
---
+++ src/tests/com/improve10x/tdd/krishna/test2problems/checkarraysortedtest/ArraySortedTest.java
@@ -0,0 +1,21 @@
+package com.improve10x.tdd.krishna.test2problems.checkarraysortedtest;
+
+import com.improve10x.tdd.krishna.test2problems.checkarraysorted.ArraySorted;
+import org.junit.jupiter.api.Test;
+
+import static org.junit.jupiter.api.Assertions.assertEquals;
+import static org.junit.jupiter.api.Assertions.assertTrue;
+
+public class ArraySortedTest {
+
+    @Test
+    public void nothing(){
+    }
+
+    @Test
+    public void givenNull_returnsTrue(){
+        ArraySorted arraySorted = new ArraySorted();
+        boolean result = arraySorted.isSortedArray(null);
+        assertTrue(result);
+    }
+}
