3b4892f0ee903bb94af1d3083725d06329a3ce73
Supriya <supriya.singamsetty@improve10x.com>
2023-08-17 12:33:57 +0530

    Added test case for given 123

--- src/tests/com/improve10x/tdd/supriya/arrayqustions/duplicateelement/DuplicateElementTest.java
+++ src/tests/com/improve10x/tdd/supriya/arrayqustions/duplicateelement/DuplicateElementTest.java
@@ -71,4 +71,11 @@ public class DuplicateElementTest {
         int[] element = duplicateElement.find(new int[]{1,2,2});
         Assertions.assertArrayEquals(new int[] {1,2}, element);
     }
+
+    @Test
+    public void given123_return12(){
+        DuplicateElement duplicateElement = new DuplicateElement();
+        int[] element = duplicateElement.find(new int[]{1,2,3});
+        Assertions.assertArrayEquals(new int[] {1,2,3}, element);
+    }
 }
