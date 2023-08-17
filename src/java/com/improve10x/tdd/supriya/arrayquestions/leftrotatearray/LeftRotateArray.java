1a94509781640aaf78b36a2c0a75227a4f7e2dbe
Supriya <supriya.singamsetty@improve10x.com>
2023-08-17 11:04:12 +0530

    Added test case for given numbers

--- src/java/com/improve10x/tdd/supriya/arrayquestions/leftrotatearray/LeftRotateArray.java
+++ src/java/com/improve10x/tdd/supriya/arrayquestions/leftrotatearray/LeftRotateArray.java
@@ -2,9 +2,21 @@ package com.improve10x.tdd.supriya.arrayquestions.leftrotatearray;
 
 public class LeftRotateArray {
     public int[] find(int[] numbers) {
-        if (numbers != null && numbers.length >=1) {
-            if (numbers[0] == numbers[0]) {
-                return new int[]{numbers[0]};
+        if (numbers != null ) {
+            if (numbers.length == 1){
+            return new int[]{numbers[0]};}
+                if (numbers.length == 2) {
+                int temp = numbers[0];
+                numbers[0] = numbers[1];
+                numbers[1] = temp;
+                return new int[]{numbers[0], numbers[1]};
+            }
+            if (numbers.length == 3) {
+                int temp = numbers[0];
+                numbers[0] = numbers[1];
+                numbers[1] = numbers[2];
+                numbers[2] = temp;
+                return new int[]{numbers[0], numbers[1], numbers[2]};
             }
         }
         return new int[]{};
--- src/tests/com/improve10x/tdd/supriya/arrayqustions/leftrottatearry/LeftRotateArrayTest.java
+++ src/tests/com/improve10x/tdd/supriya/arrayqustions/leftrottatearry/LeftRotateArrayTest.java
@@ -31,4 +31,46 @@ public class LeftRotateArrayTest {
         int[] array = leftRotateArray.find(new int[]{0});
         assertArrayEquals(new int[]{0}, array);
     }
+
+    @Test
+    public void given12_return21(){
+        LeftRotateArray leftRotateArray = new LeftRotateArray();
+        int[] array = leftRotateArray.find(new int[]{1, 2});
+        assertArrayEquals(new int[]{2, 1}, array);
+    }
+
+    @Test
+    public void given32_return23(){
+        LeftRotateArray leftRotateArray = new LeftRotateArray();
+        int[] array = leftRotateArray.find(new int[]{3,2});
+        assertArrayEquals(new int[]{2,3}, array);
+    }
+
+    @Test
+    public void given123_return231(){
+        LeftRotateArray leftRotateArray = new LeftRotateArray();
+        int[] array = leftRotateArray.find(new int[]{1,2,3});
+        assertArrayEquals(new int[]{2,3,1}, array);
+    }
+
+    @Test
+    public void given321_return213(){
+        LeftRotateArray leftRotateArray = new LeftRotateArray();
+        int[] array = leftRotateArray.find(new int[]{3,2,1});
+        assertArrayEquals(new int[]{2,1,3}, array);
+    }
+
+    @Test
+    public void given542_return425(){
+        LeftRotateArray leftRotateArray = new LeftRotateArray();
+        int[] array = leftRotateArray.find(new int[]{5,4,2});
+        assertArrayEquals(new int[]{4,2,5}, array);
+    }
+
+    @Test
+    public void given1234_return2341(){
+        LeftRotateArray leftRotateArray = new LeftRotateArray();
+        int[] array = leftRotateArray.find(new int[]{1,2,3,4});
+        assertArrayEquals(new int[]{2,3,4,1}, array);
+    }
 }
