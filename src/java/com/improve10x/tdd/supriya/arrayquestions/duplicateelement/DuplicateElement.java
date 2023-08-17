cd12bfc66bf6890d1714b3b070ef98c4c5b3d0fd
Supriya <supriya.singamsetty@improve10x.com>
2023-08-17 11:44:45 +0530

    Added test case for given null

---
+++ src/java/com/improve10x/tdd/supriya/arrayquestions/duplicateelement/DuplicateElement.java
@@ -0,0 +1,7 @@
+package com.improve10x.tdd.supriya.arrayquestions.duplicateelement;
+
+public class DuplicateElement {
+    public int[] find(int[] numbers) {
+        return new int[]{};
+    }
+}
---
+++ src/tests/com/improve10x/tdd/supriya/arrayqustions/duplicateelement/DuplicateElementTest.java
@@ -0,0 +1,21 @@
+package com.improve10x.tdd.supriya.arrayqustions.duplicateelement;
+
+import com.improve10x.tdd.supriya.arrayquestions.duplicateelement.DuplicateElement;
+import org.junit.jupiter.api.Assertions;
+import org.junit.jupiter.api.Test;
+//Remove Duplicates in-place from Sorted Array
+//Problem Statement: Given an integer array sorted in non-decreasing order, remove the duplicates
+// in place such that each unique element appears only once. The relative order of the elements
+// should be kept the same.
+public class DuplicateElementTest {
+
+    @Test
+    public void nothing(){}
+
+    @Test
+    public void givenNull_returnEmpty(){
+        DuplicateElement duplicateElement = new DuplicateElement();
+        int[] element = duplicateElement.find(null);
+        Assertions.assertArrayEquals(new int[] {}, element);
+    }
+}
