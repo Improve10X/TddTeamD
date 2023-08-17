5197ad5d902a76f182b59bc1aec89cdc80f11424
Krishna <krishna.gangimeni@improve10x.com>
2023-08-17 12:24:28 +0530

    Added testcase for input 10 in ArraySortedTest

--- src/java/com/improve10x/tdd/krishna/test2problems/checkarraysorted/ArraySorted.java
+++ src/java/com/improve10x/tdd/krishna/test2problems/checkarraysorted/ArraySorted.java
@@ -2,6 +2,9 @@ package com.improve10x.tdd.krishna.test2problems.checkarraysorted;
 
 public class ArraySorted {
     public boolean isSortedArray(int[] numbers) {
+        if(numbers != null && numbers.length > 1 && numbers[0] > numbers[1]){
+            return false;
+        }
         return true;
     }
 }
--- src/tests/com/improve10x/tdd/krishna/test2problems/checkarraysortedtest/ArraySortedTest.java
+++ src/tests/com/improve10x/tdd/krishna/test2problems/checkarraysortedtest/ArraySortedTest.java
@@ -4,6 +4,7 @@ import com.improve10x.tdd.krishna.test2problems.checkarraysorted.ArraySorted;
 import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
 
+import static org.junit.jupiter.api.Assertions.assertFalse;
 import static org.junit.jupiter.api.Assertions.assertTrue;
 
 public class ArraySortedTest {
@@ -47,5 +48,11 @@ public class ArraySortedTest {
     public void given01_returnsTrue(){
         boolean result = arraySorted.isSortedArray(new int[]{0, 1});
         assertTrue(result);
+    }
+
+    @Test
+    public void given10_returnsTrue(){
+        boolean result = arraySorted.isSortedArray(new int[]{1, 0});
+        assertFalse(result);
     }
 }
