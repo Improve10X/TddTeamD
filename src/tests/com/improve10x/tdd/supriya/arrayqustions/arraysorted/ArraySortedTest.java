e6a77fdb80827b0ce8c9a71b180d17c2ab627869
Supriya <supriya.singamsetty@improve10x.com>
2023-08-21 15:16:34 +0530

    Added test case for given 10

--- src/tests/com/improve10x/tdd/supriya/arrayqustions/arraysorted/ArraySortedTest.java
+++ src/tests/com/improve10x/tdd/supriya/arrayqustions/arraysorted/ArraySortedTest.java
@@ -2,29 +2,44 @@ package com.improve10x.tdd.supriya.arrayqustions.arraysorted;
 
 import com.improve10x.tdd.supriya.arrayquestions.arraysorted.ArraySorted;
 import org.junit.jupiter.api.Assertions;
+import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
 
 public class ArraySortedTest {
+    ArraySorted arraySorted;
+    @BeforeEach
+    public void setUp(){
+        arraySorted = new ArraySorted();
+    }
 
     @Test
     public void nothing(){}
 
     @Test
     public void givenNull_returnTrue(){
-        ArraySorted arraySorted = new ArraySorted();
         boolean sorted = arraySorted.find(null);
         Assertions.assertEquals(true, sorted);
     }
     @Test
     public void givenEmpty_returnTrue(){
-        ArraySorted arraySorted = new ArraySorted();
         boolean sorted = arraySorted.find(new int[]{});
         Assertions.assertEquals(true, sorted);
     }
     @Test
     public void given0_returnTrue(){
-        ArraySorted arraySorted = new ArraySorted();
         boolean sorted = arraySorted.find(new int[]{0});
         Assertions.assertEquals(true, sorted);
     }
+    @Test
+    public void given1_returnTrue(){
+        boolean sorted = arraySorted.find(new int[]{1});
+        Assertions.assertEquals(true, sorted);
+    }
+
+    @Test
+    public void given10_returnTrue(){
+        boolean sorted = arraySorted.find(new int[]{1,0});
+        Assertions.assertEquals(false, sorted);
+    }
+
 }
