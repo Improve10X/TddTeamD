fca45ee6e77eb512c61df0d444634285f6f67ec5
Supriya <supriya.singamsetty@improve10x.com>
2023-08-21 15:05:07 +0530

    Added test case for given null

---
+++ src/tests/com/improve10x/tdd/supriya/arrayqustions/arraysorted/ArraySortedTest.java
@@ -0,0 +1,18 @@
+package com.improve10x.tdd.supriya.arrayqustions.arraysorted;
+
+import com.improve10x.tdd.supriya.arrayquestions.arraysorted.ArraySorted;
+import org.junit.jupiter.api.Assertions;
+import org.junit.jupiter.api.Test;
+
+public class ArraySortedTest {
+
+    @Test
+    public void nothing(){}
+
+    @Test
+    public void givenNull_returnTrue(){
+        ArraySorted arraySorted = new ArraySorted();
+        boolean sorted = arraySorted.find(null);
+        Assertions.assertEquals(true, sorted);
+    }
+}
