e0f900a44526a6426b15d760c3e0e9f4da2c1800
Vani <vani.pushpala@improve10x.com>
2023-08-21 14:49:30 +0530

    Added test case and logic for input 123

--- src/tests/com/improve10x/tdd/vani/removeduplicatesfromanarray/RemoveDuplicatesTest.java
+++ src/tests/com/improve10x/tdd/vani/removeduplicatesfromanarray/RemoveDuplicatesTest.java
@@ -84,4 +84,9 @@ public class RemoveDuplicatesTest {
         int[] value = removeDuplicates.find(new int[]{1,2,2});
         assertArrayEquals(new int[]{1,2}, value);
     }
+    @Test
+    public void given123_returns123(){
+        int[] value = removeDuplicates.find(new int[]{1,2,3});
+        assertArrayEquals(new int[]{1,2,3}, value);
+    }
 }
