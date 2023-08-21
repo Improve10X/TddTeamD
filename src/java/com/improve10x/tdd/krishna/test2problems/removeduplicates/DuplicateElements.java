b1b15b431f3ecf71e1a64cc352e899582b21a963
Krishna <krishna.gangimeni@improve10x.com>
2023-08-21 12:47:46 +0530

    Added testcase and logic for input 0 in DuplicateElementsTest

--- src/java/com/improve10x/tdd/krishna/test2problems/removeduplicates/DuplicateElements.java
+++ src/java/com/improve10x/tdd/krishna/test2problems/removeduplicates/DuplicateElements.java
@@ -2,6 +2,9 @@ package com.improve10x.tdd.krishna.test2problems.removeduplicates;
 
 public class DuplicateElements {
     public int[] removeDuplicateElements(int[] numbers) {
+        if(numbers != null && numbers.length == 1){
+            return new int[]{0};
+        }
         return new int[]{};
     }
 }
