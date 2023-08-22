2bb31972b2a1c0c9ec55384c9e3156c2bc9e01d4
Krishna <krishna.gangimeni@improve10x.com>
2023-08-22 11:25:56 +0530

    Added testcase and logic for input 1 in AllZerosToTheEndTest

--- src/java/com/improve10x/tdd/krishna/test2problems/moveallzerostoend/AllZerosToTheEnd.java
+++ src/java/com/improve10x/tdd/krishna/test2problems/moveallzerostoend/AllZerosToTheEnd.java
@@ -3,7 +3,7 @@ package com.improve10x.tdd.krishna.test2problems.moveallzerostoend;
 public class AllZerosToTheEnd {
     public int[] moveAllZerosToTheEnd(int[] numbers) {
         if(numbers != null && numbers.length == 1){
-            return new int[]{0};
+            return new int[]{numbers[0]};
         }
         return new int[]{-1};
     }
