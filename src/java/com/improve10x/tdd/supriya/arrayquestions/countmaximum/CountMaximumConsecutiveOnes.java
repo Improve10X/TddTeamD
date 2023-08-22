c6b17232404cd391416f049df0af1f5af350f5ba
Supriya <supriya.singamsetty@improve10x.com>
2023-08-22 12:37:07 +0530

    Added test case for given null

---
+++ src/java/com/improve10x/tdd/supriya/arrayquestions/countmaximum/CountMaximumConsecutiveOnes.java
@@ -0,0 +1,7 @@
+package com.improve10x.tdd.supriya.arrayquestions.countmaximum;
+
+public class CountMaximumConsecutiveOnes {
+    public int find(int[] numbers) {
+        return 0;
+    }
+}
---
+++ src/tests/com/improve10x/tdd/supriya/arrayqustions/countMaximum/CountMaximumConsecutiveOnesTest.java
@@ -0,0 +1,19 @@
+package com.improve10x.tdd.supriya.arrayqustions.countMaximum;
+
+import com.improve10x.tdd.supriya.arrayquestions.countmaximum.CountMaximumConsecutiveOnes;
+import org.junit.jupiter.api.Assertions;
+import org.junit.jupiter.api.Test;
+
+public class CountMaximumConsecutiveOnesTest {
+
+    @Test
+    public void nothing(){}
+
+    @Test
+    public void givenNull_returnsZero(){
+        CountMaximumConsecutiveOnes countMaximumConsecutiveOnes = new CountMaximumConsecutiveOnes();
+        int count = countMaximumConsecutiveOnes.find(null);
+        Assertions.assertEquals(0, count);
+    }
+
+}
