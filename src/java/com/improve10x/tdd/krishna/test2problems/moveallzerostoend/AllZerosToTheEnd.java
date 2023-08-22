7d23f346ee438e8f12c54f46d70c88c4c911d36f
Krishna <krishna.gangimeni@improve10x.com>
2023-08-22 11:18:11 +0530

    Added testcase and logic for input null in AllZerosToTheEndTest

---
+++ src/java/com/improve10x/tdd/krishna/test2problems/moveallzerostoend/AllZerosToTheEnd.java
@@ -0,0 +1,7 @@
+package com.improve10x.tdd.krishna.test2problems.moveallzerostoend;
+
+public class AllZerosToTheEnd {
+    public int[] moveAllZerosToTheEnd(int[] numbers) {
+        return new int[]{-1};
+    }
+}
