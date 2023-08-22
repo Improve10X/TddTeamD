ca830cc0b28b752db74a0ff61368a889fdd66efa
Supriya <supriya.singamsetty@improve10x.com>
2023-08-22 12:58:13 +0530

    Added test case for given 1

--- src/tests/com/improve10x/tdd/supriya/arrayqustions/countMaximum/CountMaximumConsecutiveOnesTest.java
+++ src/tests/com/improve10x/tdd/supriya/arrayqustions/countMaximum/CountMaximumConsecutiveOnesTest.java
@@ -2,32 +2,40 @@ package com.improve10x.tdd.supriya.arrayqustions.countMaximum;
 
 import com.improve10x.tdd.supriya.arrayquestions.countmaximum.CountMaximumConsecutiveOnes;
 import org.junit.jupiter.api.Assertions;
+import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
 
 public class CountMaximumConsecutiveOnesTest {
+    CountMaximumConsecutiveOnes countMaximumConsecutiveOnes;
+    @BeforeEach
+    public void setUp(){
+        countMaximumConsecutiveOnes = new CountMaximumConsecutiveOnes();
+    }
 
     @Test
     public void nothing(){}
 
     @Test
     public void givenNull_returnsZero(){
-        CountMaximumConsecutiveOnes countMaximumConsecutiveOnes = new CountMaximumConsecutiveOnes();
         int count = countMaximumConsecutiveOnes.find(null);
         Assertions.assertEquals(0, count);
     }
 
     @Test
     public void givenEmpty_returnsZero(){
-        CountMaximumConsecutiveOnes countMaximumConsecutiveOnes = new CountMaximumConsecutiveOnes();
         int count = countMaximumConsecutiveOnes.find(new int[]{});
         Assertions.assertEquals(0, count);
     }
 
     @Test
     public void givenZero_returnsZero(){
-        CountMaximumConsecutiveOnes countMaximumConsecutiveOnes = new CountMaximumConsecutiveOnes();
         int count = countMaximumConsecutiveOnes.find(new int[]{0});
         Assertions.assertEquals(0, count);
+    }
+    @Test
+    public void given1_returns1(){
+        int count = countMaximumConsecutiveOnes.find(new int[]{1});
+        Assertions.assertEquals(1, count);
     }
 
 }
