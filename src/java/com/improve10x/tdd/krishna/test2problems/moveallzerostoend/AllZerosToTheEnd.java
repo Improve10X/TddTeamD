01f3add3842b4f5a09a71a2e32e7fa02ae91dd33
Krishna <krishna.gangimeni@improve10x.com>
2023-08-22 11:32:22 +0530

    Added testcase and logic for input 01 in AllZerosToTheEndTest

--- src/java/com/improve10x/tdd/krishna/test2problems/moveallzerostoend/AllZerosToTheEnd.java
+++ src/java/com/improve10x/tdd/krishna/test2problems/moveallzerostoend/AllZerosToTheEnd.java
@@ -3,7 +3,15 @@ package com.improve10x.tdd.krishna.test2problems.moveallzerostoend;
 public class AllZerosToTheEnd {
     public int[] moveAllZerosToTheEnd(int[] numbers) {
         if(numbers != null && numbers.length == 1){
-            return new int[]{numbers[0]};
+            return numbers;
+        }
+        if(numbers != null && numbers.length == 2){
+            if(numbers[0] == 0){
+                int temp = numbers[0];
+                numbers[0] = numbers[1];
+                numbers[1] = temp;
+                return numbers;
+            }
         }
         return new int[]{-1};
     }
--- src/tests/com/improve10x/tdd/krishna/test2problems/moveallzerostoendtest/AllZerosToTheEndTest.java
+++ src/tests/com/improve10x/tdd/krishna/test2problems/moveallzerostoendtest/AllZerosToTheEndTest.java
@@ -42,4 +42,10 @@ public class AllZerosToTheEndTest {
         int[] result = allZerosToTheEnd.moveAllZerosToTheEnd(new int[]{1});
         assertArrayEquals(new int[]{1}, result);
     }
+
+    @Test
+    public void given01_returns10(){
+        int[] result = allZerosToTheEnd.moveAllZerosToTheEnd(new int[]{0, 1});
+        assertArrayEquals(new int[]{1, 0}, result);
+    }
 }
