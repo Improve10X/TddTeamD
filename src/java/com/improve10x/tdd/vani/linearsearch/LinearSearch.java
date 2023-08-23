851e9d18591d4a29efa18658a080667929edc37d
Vani <vani.pushpala@improve10x.com>
2023-08-23 17:52:25 +0530

    Added Testcase for input null and 1 in LinearSearch in c

---
+++ src/java/com/improve10x/tdd/vani/linearsearch/LinearSearch.java
@@ -0,0 +1,8 @@
+package com.improve10x.tdd.vani.linearsearch;
+
+public class LinearSearch {
+
+    public int find(int[] numbers, int n) {
+        return -1;
+    }
+}
--- src/java/com/improve10x/tdd/vani/reversewordsinastring/ReverseWords.java
+++ src/java/com/improve10x/tdd/vani/reversewordsinastring/ReverseWords.java
@@ -3,22 +3,20 @@ package com.improve10x.tdd.vani.reversewordsinastring;
 public class ReverseWords {
     public String find(String sentence) {
         if (sentence != null) {
-
-            if (sentence.equals("A B")) {
-                return "B A";
-            }
-
-            if (sentence.equals("10x Improve")){
-                return "Improve 10x";
-            }
-            if (sentence.equals("Mom Dad")){
-                return "Dad Mom";
-            }
-            String reverseElements = "";
+//            if (sentence.equals("A B")) {
+//                return "B A";
+//            }
+//            if (sentence.equals("10x Improve")){
+//                return "Improve 10x";
+//            }
+//            if (sentence.equals("Mom Dad")){
+//                return "Dad Mom";
+//            }
+            String reverseWords = "";
             for (int i = sentence.length() - 1; i >= 0; i--){
 
             }
-            return reverseElements;
+            return reverseWords;
         }
         return "";
     }
