88e9cebf0146775f0a3fa212a4f034027bca41aa
Supriya <supriya.singamsetty@improve10x.com>
2023-08-23 17:55:11 +0530

    Added test case for given 12345

--- src/java/com/improve10x/tdd/supriya/arrayquestions/linearsearch/LinearSearch.java
+++ src/java/com/improve10x/tdd/supriya/arrayquestions/linearsearch/LinearSearch.java
@@ -3,19 +3,30 @@ package com.improve10x.tdd.supriya.arrayquestions.linearsearch;
 public class LinearSearch {
     public int find(int[] numbers, int n ) {
         if (numbers != null && numbers.length>=1) {
-            if (numbers.length == 2) {
-                if (numbers[1] == n) {
-                    return 1;
+//            if (numbers.length == 2) {
+//                if (numbers[1] == n) {
+//                    return 1;
+//                }
+//            }
+//            if (numbers.length == 3){
+//                if (numbers[0] == n){
+//                    return 0;
+//                }
+//                if (numbers[2] == n){
+//                    return 2;
+//                }
+//            }
+//            if (numbers.length == 4){
+//                if (numbers[3] == n){
+//                    return 3;
+//                }
+//            }
+            for (int i = 0; i <numbers.length; i++){
+                if (numbers[i] == n){
+                    return i;
                 }
             }
-            if (numbers.length == 3){
-                if (numbers[0] == n){
-                    return 0;
-                }
-                if (numbers[2] == n){
-                    return 2;
-                }
-            }
+            return -1;
         }
         return -1;
     }
