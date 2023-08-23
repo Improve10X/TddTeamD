23e160a00a7991705cc9459343418be6776a2aa5
Supriya <supriya.singamsetty@improve10x.com>
2023-08-23 16:05:08 +0530

    Added test case for given consecutives numbers

--- src/java/com/improve10x/tdd/supriya/arrayquestions/countmaximum/CountMaximumConsecutiveOnes.java
+++ src/java/com/improve10x/tdd/supriya/arrayquestions/countmaximum/CountMaximumConsecutiveOnes.java
@@ -2,20 +2,19 @@ package com.improve10x.tdd.supriya.arrayquestions.countmaximum;
 
 public class CountMaximumConsecutiveOnes {
     public int find(int[] numbers) {
-        if (numbers != null && numbers.length >=1) {
-//            if (numbers[0] ==1){
-//                return 1;
-//            }
-            int count = 0;//101101
+        if (numbers == null || numbers.length == 0) {
+            return 0;
+        }
+            int count = 0;
+            int maximum = 0;
             for (int i = 0; i < numbers.length; i ++){
-                if (numbers[i] == 1 ){
+                if (numbers[i] == 1 ) {
                     count++;
-                }else {
+                } else {
                     count = 0;
                 }
+                maximum = Math.max(maximum,count);
             }
-            return count;
-        }
-        return 0;
+            return maximum;
     }
 }
--- src/tests/com/improve10x/tdd/supriya/arrayqustions/countMaximum/CountMaximumConsecutiveOnesTest.java
+++ src/tests/com/improve10x/tdd/supriya/arrayqustions/countMaximum/CountMaximumConsecutiveOnesTest.java
@@ -6,6 +6,9 @@ import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
 
 public class CountMaximumConsecutiveOnesTest {
+//    Count Maximum Consecutive One’s in the array
+//    Problem Statement: Given an array that contains only 1 and 0 return the count of maximum
+//    consecutive ones in the array.
     CountMaximumConsecutiveOnes countMaximumConsecutiveOnes;
     @BeforeEach
     public void setUp(){
