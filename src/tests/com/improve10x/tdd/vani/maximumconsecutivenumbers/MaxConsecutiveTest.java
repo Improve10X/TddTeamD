7bc8959b8b8ed6e40c261047e85c8dd197053b11
Vani <vani.pushpala@improve10x.com>
2023-08-23 16:09:53 +0530

    Added test case for input empty

--- src/tests/com/improve10x/tdd/vani/maximumconsecutivenumbers/MaxConsecutiveTest.java
+++ src/tests/com/improve10x/tdd/vani/maximumconsecutivenumbers/MaxConsecutiveTest.java
@@ -1,5 +1,6 @@
 package com.improve10x.tdd.vani.maximumconsecutivenumbers;
 
+import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
 //Count Maximum Consecutive One’s in the array
 //        Problem Statement: Given an array that contains only 1 and 0 return the count of maximum consecutive ones in the array.
@@ -20,12 +21,22 @@ import org.junit.jupiter.api.Test;
 import static org.junit.jupiter.api.Assertions.assertEquals;
 
 public class MaxConsecutiveTest {
+    private MaxConsecutive maxConsecutive;
+    @BeforeEach
+    public void setUp(){
+        maxConsecutive = new MaxConsecutive();
+    }
     @Test
     public void nothing(){}
     @Test
-    public void givenNull_returnsMinus0(){
-        MaxConsecutive maxConsecutive = new MaxConsecutive();
+    public void givenNull_returns0(){
         int value = maxConsecutive.find(null);
         assertEquals(0,value);
     }
+    @Test
+    public void givenEmpty_returns0(){
+        int value = maxConsecutive.find(new int[]{0});
+        assertEquals(0,value);
+    }
+
 }
