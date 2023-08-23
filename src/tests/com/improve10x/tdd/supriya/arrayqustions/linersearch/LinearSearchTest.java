012b0f376d618f298dd692d3f2dc1a5a4373a383
Supriya <supriya.singamsetty@improve10x.com>
2023-08-23 16:32:43 +0530

    Added test case for given null

---
+++ src/tests/com/improve10x/tdd/supriya/arrayqustions/linersearch/LinearSearchTest.java
@@ -0,0 +1,18 @@
+package com.improve10x.tdd.supriya.arrayqustions.linersearch;
+
+import com.improve10x.tdd.supriya.arrayquestions.linearsearch.LinearSearch;
+import org.junit.jupiter.api.Assertions;
+import org.junit.jupiter.api.Test;
+
+public class LinearSearchTest {
+
+    @Test
+    public void nothing(){}
+
+    @Test
+    public void givenNull_returnMinusOne(){
+        LinearSearch linearSearch = new LinearSearch();
+        int search = linearSearch.find(null);
+        Assertions.assertEquals(-1, search);
+    }
+}
