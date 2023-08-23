1f5a6b4ff3c9c5b8ba9890e68bf53b4d25719621
Krishna <krishna.gangimeni@improve10x.com>
2023-08-23 12:38:07 +0530

    Code refactored

--- src/java/com/improve10x/tdd/krishna/test2problems/removeduplicates/DuplicateElements.java
+++ src/java/com/improve10x/tdd/krishna/test2problems/removeduplicates/DuplicateElements.java
@@ -1,55 +1,22 @@
 package com.improve10x.tdd.krishna.test2problems.removeduplicates;
 
+import java.util.ArrayList;
+
 public class DuplicateElements {
     public int[] removeDuplicateElements(int[] numbers) {
         if (numbers != null && numbers.length >= 1) {
-            if (numbers.length == 1) {
-                return numbers;
+            ArrayList<Integer> result = new ArrayList<>();
+            result.add(numbers[0]);
+            for (int i = 1; i < numbers.length; i++) {
+                if (numbers[i] != result.get(result.size() - 1)) {
+                    result.add(numbers[i]);
+                }
             }
-            if (numbers.length == 2) {
-                if (numbers[0] == numbers[1]) {
-                    return new int[]{numbers[0]};
-                }
-                if (numbers[0] > numbers[1]) {
-                    int temp = numbers[0];
-                    numbers[0] = numbers[1];
-                    numbers[1] = temp;
-                }
-                return numbers;
+            int[] resultArray = new int[result.size()];
+            for (int i = 0; i < result.size(); i++) {
+                resultArray[i] = result.get(i);
             }
-            if (numbers.length == 3) {
-                if (numbers[0] == numbers[1] && numbers[1] == numbers[2]) {
-                    return new int[]{numbers[0]};
-                }
-                if (numbers[0] == numbers[1]) {
-                    return new int[]{numbers[0], numbers[2]};
-                }
-                if (numbers[0] > numbers[1] && numbers[1] == numbers[2]) {
-                    int temp = numbers[0];
-                    numbers[0] = numbers[1];
-                    numbers[1] = temp;
-                    return new int[]{numbers[0], numbers[1]};
-                }
-                if (numbers[0] == numbers[2] && numbers[0] < numbers[1]) {
-                    return new int[]{numbers[0], numbers[1]};
-                }
-                return numbers;
-            }
-            if (numbers.length == 4) {
-                if (numbers[0] == numbers[1] && numbers[0] == numbers[2] && numbers[0] == numbers[3]) {
-                    return new int[]{numbers[0]};
-                }
-                if (numbers[0] == numbers[1] && numbers[2] != numbers[3]) {
-                    return new int[]{numbers[0], numbers[2], numbers[3]};
-                }
-                if (numbers[0] == numbers[3] && numbers[1] < numbers[2]) {
-                    return new int[]{numbers[0], numbers[1], numbers[2]};
-                }
-                if (numbers[0] < numbers[1] && numbers[2] == numbers[3]) {
-                    return new int[]{numbers[2], numbers[0], numbers[1]};
-                }
-                return numbers;
-            }
+            return resultArray;
         }
         return new int[]{};
     }
--- src/tests/com/improve10x/tdd/krishna/test2problems/moveallzerostoendtest/AllZerosToTheEndTest.java
+++ src/tests/com/improve10x/tdd/krishna/test2problems/moveallzerostoendtest/AllZerosToTheEndTest.java
@@ -2,110 +2,118 @@ package com.improve10x.tdd.krishna.test2problems.moveallzerostoendtest;
 
 import com.improve10x.tdd.krishna.test2problems.moveallzerostoend.AllZerosToTheEnd;
 import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Disabled;
 import org.junit.jupiter.api.Test;
 
 import static org.junit.jupiter.api.Assertions.assertArrayEquals;
 
 public class AllZerosToTheEndTest {
 
-    private  AllZerosToTheEnd allZerosToTheEnd;
+    private AllZerosToTheEnd allZerosToTheEnd;
 
     @BeforeEach
-    public void setup(){
+    public void setup() {
         allZerosToTheEnd = new AllZerosToTheEnd();
     }
 
     @Test
-    public void nothing(){
+    public void nothing() {
     }
 
     @Test
-    public void givenNull_returnsMinusOne(){
+    public void givenNull_returnsMinusOne() {
         int[] result = allZerosToTheEnd.moveAllZerosToTheEnd(null);
         assertArrayEquals(new int[]{-1}, result);
     }
 
     @Test
-    public void givenEmpty_returnsMinusOne(){
+    public void givenEmpty_returnsMinusOne() {
         int[] result = allZerosToTheEnd.moveAllZerosToTheEnd(new int[]{});
         assertArrayEquals(new int[]{-1}, result);
     }
 
     @Test
-    public void given0_returns0(){
+    public void given0_returns0() {
         int[] result = allZerosToTheEnd.moveAllZerosToTheEnd(new int[]{0});
         assertArrayEquals(new int[]{0}, result);
     }
 
     @Test
-    public void given1_returns1(){
+    public void given1_returns1() {
         int[] result = allZerosToTheEnd.moveAllZerosToTheEnd(new int[]{1});
         assertArrayEquals(new int[]{1}, result);
     }
 
     @Test
-    public void given01_returns10(){
+    public void given01_returns10() {
         int[] result = allZerosToTheEnd.moveAllZerosToTheEnd(new int[]{0, 1});
         assertArrayEquals(new int[]{1, 0}, result);
     }
 
     @Test
-    public void given10_returns10(){
+    public void given10_returns10() {
         int[] result = allZerosToTheEnd.moveAllZerosToTheEnd(new int[]{1, 0});
         assertArrayEquals(new int[]{1, 0}, result);
     }
 
     @Test
-    public void given12_returns12(){
+    public void given12_returns12() {
         int[] result = allZerosToTheEnd.moveAllZerosToTheEnd(new int[]{1, 2});
         assertArrayEquals(new int[]{1, 2}, result);
     }
 
     @Test
-    public void given00_returns00(){
+    public void given00_returns00() {
         int[] result = allZerosToTheEnd.moveAllZerosToTheEnd(new int[]{0, 0});
         assertArrayEquals(new int[]{0, 0}, result);
     }
 
     @Test
-    public void given012_returns120(){
+    public void given012_returns120() {
         int[] result = allZerosToTheEnd.moveAllZerosToTheEnd(new int[]{0, 1, 2});
         assertArrayEquals(new int[]{1, 2, 0}, result);
     }
 
     @Test
-    public void given201_returns210(){
+    public void given201_returns210() {
         int[] result = allZerosToTheEnd.moveAllZerosToTheEnd(new int[]{2, 0, 1});
         assertArrayEquals(new int[]{2, 1, 0}, result);
     }
 
     @Test
-    public void given120_returns120(){
+    public void given120_returns120() {
         int[] result = allZerosToTheEnd.moveAllZerosToTheEnd(new int[]{1, 2, 0});
         assertArrayEquals(new int[]{1, 2, 0}, result);
     }
 
     @Test
-    public void given001_returns100(){
+    public void given001_returns100() {
         int[] result = allZerosToTheEnd.moveAllZerosToTheEnd(new int[]{0, 0, 1});
         assertArrayEquals(new int[]{1, 0, 0}, result);
     }
 
     @Test
-    public void given010_returns100(){
+    public void given010_returns100() {
         int[] result = allZerosToTheEnd.moveAllZerosToTheEnd(new int[]{0, 1, 0});
         assertArrayEquals(new int[]{1, 0, 0}, result);
     }
 
     @Test
-    public void given000_returns000(){
+    public void given000_returns000() {
         int[] result = allZerosToTheEnd.moveAllZerosToTheEnd(new int[]{0, 0, 0});
         assertArrayEquals(new int[]{0, 0, 0}, result);
     }
 
     @Test
-    public void given132_returns132(){
+    public void given132_returns132() {
         int[] result = allZerosToTheEnd.moveAllZerosToTheEnd(new int[]{1, 3, 2});
         assertArrayEquals(new int[]{1, 3, 2}, result);
+    }
+
+    @Disabled
+    @Test
+    public void given1032_returns1320() {
+        int[] result = allZerosToTheEnd.moveAllZerosToTheEnd(new int[]{1, 0, 3, 2});
+        assertArrayEquals(new int[]{1, 3, 2, 0}, result);
     }
 }
