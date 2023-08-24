9540ec8aa9b426a0f287923d9d3f54a44c2c77de
Krishna <krishna.gangimeni@improve10x.com>
2023-08-24 12:42:43 +0530

    Added testcase and logic for input A in ReverseWordsTest.java

--- src/java/com/improve10x/tdd/krishna/test2problems/reversewordsinastring/ReverseWords.java
+++ src/java/com/improve10x/tdd/krishna/test2problems/reversewordsinastring/ReverseWords.java
@@ -3,6 +3,9 @@ package com.improve10x.tdd.krishna.test2problems.reversewordsinastring;
 public class ReverseWords {
 
     public String findReverse(String words){
+        if(words != null && words.length() == 1){
+            return words;
+        }
         return "";
     }
 }
--- src/tests/com/improve10x/tdd/krishna/test2problems/reversewordsinastringtest/ReverseWordsTest.java
+++ src/tests/com/improve10x/tdd/krishna/test2problems/reversewordsinastringtest/ReverseWordsTest.java
@@ -1,11 +1,19 @@
 package com.improve10x.tdd.krishna.test2problems.reversewordsinastringtest;
 
 import com.improve10x.tdd.krishna.test2problems.reversewordsinastring.ReverseWords;
+import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
 
 import static org.junit.jupiter.api.Assertions.assertEquals;
 
 public class ReverseWordsTest {
+
+    private  ReverseWords reverseWords;
+
+    @BeforeEach
+    public void setup(){
+        reverseWords = new ReverseWords();
+    }
 
     @Test
     public void nothing(){
@@ -13,15 +21,19 @@ public class ReverseWordsTest {
 
     @Test
     public void givenNull_returnsEmpty(){
-        ReverseWords reverseWords = new ReverseWords();
         String reversed = reverseWords.findReverse(null);
         assertEquals("", reversed);
     }
 
     @Test
     public void givenEmpty_returnsEmpty(){
-        ReverseWords reverseWords = new ReverseWords();
         String reversed = reverseWords.findReverse("");
         assertEquals("", reversed);
+    }
+
+    @Test
+    public void givenA_returnsA(){
+        String reversed = reverseWords.findReverse("A");
+        assertEquals("A", reversed);
     }
 }
