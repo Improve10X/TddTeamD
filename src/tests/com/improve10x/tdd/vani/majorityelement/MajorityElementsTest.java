62c4367b243c2296ce9f1ddb51f7ac07448691db
Vani <vani.pushpala@improve10x.com>
2023-08-24 16:02:15 +0530

    Added Testcase for input 0

--- src/tests/com/improve10x/tdd/vani/majorityelement/MajorityElementsTest.java
+++ src/tests/com/improve10x/tdd/vani/majorityelement/MajorityElementsTest.java
@@ -42,4 +42,9 @@ public class MajorityElementsTest {
         int value = majorityElements.find(new int[]{});
         assertEquals(-1,value);
     }
+    @Test
+    public void given0_returnsMinus1(){
+        int value = majorityElements.find(new int[]{0});
+        assertEquals(-1,value);
+    }
 }
