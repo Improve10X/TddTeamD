0887e83aec44c0416e3500b5f8b3ff06bd6b9d78
Krishna <krishna.gangimeni@improve10x.com>
2023-08-24 12:34:34 +0530

    Added testcase and logic for input null in ReverseWords.java

---
+++ src/java/com/improve10x/tdd/krishna/test2problems/reversewordsinastring/ReverseWords.java
@@ -0,0 +1,8 @@
+package com.improve10x.tdd.krishna.test2problems.reversewordsinastring;
+
+public class ReverseWords {
+
+    public String findReverse(String words){
+        return "";
+    }
+}
---
+++ src/tests/com/improve10x/tdd/krishna/test2problems/reversewordsinastringtest/ReverseWordsTest.java
@@ -0,0 +1,20 @@
+package com.improve10x.tdd.krishna.test2problems.reversewordsinastringtest;
+
+import com.improve10x.tdd.krishna.test2problems.reversewordsinastring.ReverseWords;
+import org.junit.jupiter.api.Test;
+
+import static org.junit.jupiter.api.Assertions.assertEquals;
+
+public class ReverseWordsTest {
+
+    @Test
+    public void nothing(){
+    }
+
+    @Test
+    public void givenNull_returnsEmpty(){
+        ReverseWords reverseWords = new ReverseWords();
+        String reversed = reverseWords.findReverse(null);
+        assertEquals("", reversed);
+    }
+}
