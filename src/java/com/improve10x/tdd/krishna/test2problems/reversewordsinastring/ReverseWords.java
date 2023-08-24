f5b409b49d3c86501cb0b8fd397b6af67002a11b
Krishna <krishna.gangimeni@improve10x.com>
2023-08-24 12:46:47 +0530

    Added testcase and logic for input Is in ReverseWordsTest.java

--- src/java/com/improve10x/tdd/krishna/test2problems/reversewordsinastring/ReverseWords.java
+++ src/java/com/improve10x/tdd/krishna/test2problems/reversewordsinastring/ReverseWords.java
@@ -3,7 +3,7 @@ package com.improve10x.tdd.krishna.test2problems.reversewordsinastring;
 public class ReverseWords {
 
     public String findReverse(String words){
-        if(words != null && words.length() == 1){
+        if(words != null && words.length() >= 1){
             return words;
         }
         return "";
