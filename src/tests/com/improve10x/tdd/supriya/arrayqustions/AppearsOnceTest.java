d0d17d1c852bcfe4c9e695daa708635866ffada6
Supriya <supriya.singamsetty@improve10x.com>
2023-08-24 16:06:37 +0530

    Added test case for given AppearsOnce

--- src/tests/com/improve10x/tdd/supriya/arrayqustions/AppearsOnceTest.java
+++ src/tests/com/improve10x/tdd/supriya/arrayqustions/AppearsOnceTest.java
@@ -2,58 +2,71 @@ package com.improve10x.tdd.supriya.arrayqustions;
 
 import com.improve10x.tdd.supriya.arrayquestions.AppearsOnce;
 import org.junit.jupiter.api.Assertions;
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Disabled;
 import org.junit.jupiter.api.Test;
 
+import static org.junit.jupiter.api.Assertions.assertEquals;
+
+//Problem Statement: Given a non-empty array of integers arr, every element appears twice except for one. Find that single one.
 public class AppearsOnceTest {
+    AppearsOnce appearsOnce;
+
+    @BeforeEach
+    public void setUp() {
+        appearsOnce = new AppearsOnce();
+
+    }
 
     @Test
-    public void nothing(){}
+    public void nothing() {
+    }
 
     @Test
-    public void givenNull_returnMinusOne(){
-        AppearsOnce appearsOnce = new AppearsOnce();
+    public void givenNull_returnMinusOne() {
         int singleElement = appearsOnce.find(null);
-        Assertions.assertEquals(-1, singleElement);
+        assertEquals(-1, singleElement);
     }
 
     @Test
-    public void givenEmpty_returnMinusOne(){
-        AppearsOnce appearsOnce = new AppearsOnce();
+    public void givenEmpty_returnMinusOne() {
         int singleElement = appearsOnce.find(new int[]{});
-        Assertions.assertEquals(-1, singleElement);
+        assertEquals(-1, singleElement);
     }
 
     @Test
-    public void given0_return0(){
-        AppearsOnce appearsOnce = new AppearsOnce();
+    public void given0_return0() {
         int singleElement = appearsOnce.find(new int[]{0});
-        Assertions.assertEquals(0, singleElement);
+        assertEquals(0, singleElement);
     }
 
     @Test
-    public void given1_return1(){
-        AppearsOnce appearsOnce = new AppearsOnce();
+    public void given1_return1() {
         int singleElement = appearsOnce.find(new int[]{1});
-        Assertions.assertEquals(1, singleElement);
+        assertEquals(1, singleElement);
     }
 
     @Test
-    public void given001_return1(){
-        AppearsOnce appearsOnce = new AppearsOnce();
-        int singleElement = appearsOnce.find(new int[]{0,0,1});
-        Assertions.assertEquals(1, singleElement);
-    }
-    @Test
-    public void given110_return0(){
-        AppearsOnce appearsOnce = new AppearsOnce();
-        int singleElement = appearsOnce.find(new int[]{1,1,0});
-        Assertions.assertEquals(0, singleElement);
+    public void given001_return1() {
+        int singleElement = appearsOnce.find(new int[]{0, 0, 1});
+        assertEquals(1, singleElement);
     }
 
     @Test
-    public void given11002_return2(){
-        AppearsOnce appearsOnce = new AppearsOnce();
-        int singleElement = appearsOnce.find(new int[]{1,1,0,0,2});
-        Assertions.assertEquals(2, singleElement);
+    public void given110_return0() {
+        int singleElement = appearsOnce.find(new int[]{1, 1, 0});
+        assertEquals(0, singleElement);
+    }
+
+    @Test
+    public void given11002_return2() {
+        int singleElement = appearsOnce.find(new int[]{1, 1, 0, 0, 2});
+        assertEquals(2, singleElement);
+    }
+
+    @Test
+    public void given10012_return2() {
+        int singleElement = appearsOnce.find(new int[]{1, 0, 0, 1, 2});
+        assertEquals(2, singleElement);
     }
 }
