a7d3fd869bcc04c6a71b9398b36af41ccfdbdb4b
Vani <vani.pushpala@improve10x.com>
2023-08-29 17:47:15 +0530

    Added Testcase and logic for input 0 in Majority

--- src/java/com/improve10x/tdd/vani/majorityelement/MajorityElements.java
+++ src/java/com/improve10x/tdd/vani/majorityelement/MajorityElements.java
@@ -2,6 +2,11 @@ package com.improve10x.tdd.vani.majorityelement;
 
 public class MajorityElements {
     public int find(int[] numbers) {
+        if (numbers != null) {
+            if (numbers.length > numbers.length / 2) {
+                return 0;
+            }
+        }
         return -1;
     }
 }
