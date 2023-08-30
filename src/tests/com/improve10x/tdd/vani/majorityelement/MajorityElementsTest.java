28cb363a56e34e3f984af0162fd76de9e5e8516a
Vani <vani.pushpala@improve10x.com>
2023-08-30 10:29:10 +0530

    Added Testcase and logic for input 123

--- src/tests/com/improve10x/tdd/vani/majorityelement/MajorityElementsTest.java
+++ src/tests/com/improve10x/tdd/vani/majorityelement/MajorityElementsTest.java
@@ -67,4 +67,9 @@ public class MajorityElementsTest {
         int value = majorityElements.find(new int[]{0,0});
         assertEquals(0, value);
     }
+    @Test
+    public void given123_returnsMinus1(){
+        int value = majorityElements.find(new int[]{1,2,3});
+        assertEquals(-1, value);
+    }
 }
