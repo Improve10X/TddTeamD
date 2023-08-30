c9ad0fad14e25c128f07008e8864ee6f152d7643
Supriya <supriya.singamsetty@improve10x.com>
2023-08-30 11:05:36 +0530

    Added test case for given 01

--- src/java/com/improve10x/tdd/supriya/arrayquestions/moveszerostoend/MovesZerosToEnd.java
+++ src/java/com/improve10x/tdd/supriya/arrayquestions/moveszerostoend/MovesZerosToEnd.java
@@ -2,6 +2,16 @@ package com.improve10x.tdd.supriya.arrayquestions.moveszerostoend;
 
 public class MovesZerosToEnd {
     public int[] find(int[] numbers) {
+        if (numbers == null || numbers.length == 0) {
+            return new int[]{};
+        }
+        if (numbers.length == 1){
+            return new int[]{0};
+        }
+        if (numbers[0]<numbers[1]){
+            return new int[]{1,0};
+        }
         return new int[]{};
     }
+
 }
