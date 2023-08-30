daeb9799d01c42275f8946bd4b69c0c623a8f1ec
Vani <vani.pushpala@improve10x.com>
2023-08-30 16:35:31 +0530

    Added Testcase and logic for input some changes

--- src/tests/com/improve10x/tdd/vani/leadersarray/LeadersArrayTest.java
+++ src/tests/com/improve10x/tdd/vani/leadersarray/LeadersArrayTest.java
@@ -1,15 +1,23 @@
 package com.improve10x.tdd.vani.leadersarray;
 
+import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
 
 import static org.junit.jupiter.api.Assertions.assertArrayEquals;
 
 public class LeadersArrayTest {
+    private LeadersArray leadersArray;
+
+    @BeforeEach
+    public void setUp(){
+        leadersArray = new LeadersArray();
+    }
+
     @Test
     public void nothing(){}
+
     @Test
     public void givenNull_returnsEmpty(){
-        LeadersArray leadersArray = new LeadersArray();
         int[] values = leadersArray.find(null);
         assertArrayEquals(new int[]{}, values);
     }
