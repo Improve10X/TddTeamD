27ec106fbba8dbf339e8fa796048499250ccffe0
Supriya <supriya.singamsetty@improve10x.com>
2023-08-30 12:25:23 +0530

    Added test case for given 102

--- src/tests/com/improve10x/tdd/supriya/arrayqustions/MovesZerosToEndTest.java
+++ src/tests/com/improve10x/tdd/supriya/arrayqustions/MovesZerosToEndTest.java
@@ -42,5 +42,18 @@ public class MovesZerosToEndTest {
         int[] zeros = movesZerosToEnd.find(new int[]{0,2});
         Assertions.assertArrayEquals(new int[]{2,0}, zeros);
     }
+    @Test
+    public void given012_return120(){
+        MovesZerosToEnd movesZerosToEnd = new MovesZerosToEnd();
+        int[] zeros = movesZerosToEnd.find(new int[]{0,1,2});
+        Assertions.assertArrayEquals(new int[]{1,2,0}, zeros);
+    }
+
+    @Test
+    public void given102_return120(){
+        MovesZerosToEnd movesZerosToEnd = new MovesZerosToEnd();
+        int[] zeros = movesZerosToEnd.find(new int[]{1,0,2});
+        Assertions.assertArrayEquals(new int[]{1,2,0}, zeros);
+    }
 
 }
