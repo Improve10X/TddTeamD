9f2be9ab197ec2ca06c02e387455ccaeab5ff26a
Vani <vani.pushpala@improve10x.com>
2023-09-01 10:45:47 +0530

    Added Testcase and logic for input 111

--- src/java/com/improve10x/tdd/vani/leadersarray/LeadersArray.java
+++ src/java/com/improve10x/tdd/vani/leadersarray/LeadersArray.java
@@ -31,6 +31,9 @@ public class LeadersArray {
             if (numbers[0] < numbers[1] && numbers[0] > numbers[2]){
                 return new int[]{numbers[1],numbers[2]};
             }
+            if (numbers[0] == numbers[1] && numbers[0] == numbers[2]){
+                return new int[]{numbers[0],numbers[1],numbers[2]};
+            }
 
         }
         return new int[]{};
